fcef96fef25c49ca40186b93d04d7a32b21ef965
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2012-09-25 15:53:32 +0200

    Removed unused code.

--- statistics-model/src/main/java/se/inera/statistics/model/entity/MedicalCertificateEntity.java
+++ statistics-model/src/main/java/se/inera/statistics/model/entity/MedicalCertificateEntity.java
@@ -24,15 +24,6 @@ public class MedicalCertificateEntity {
 	@Column(nullable=false)
 	private Long careUnitId;
 
-//	@Column
-//	private boolean actualSicknessProcess;
-	
-//	@Column
-//	private boolean examinationResults;
-	
-//	@Column
-//	private boolean limitedActivity;
-
 	@Column(nullable=false)
 	private Long startDate;
 	
@@ -44,12 +35,9 @@ public class MedicalCertificateEntity {
 	
 	@Column(nullable=false)
 	private Boolean basedOnTelephoneContact;
-	
-//	@Column
-//	private boolean basedOnJournal;
-	
-//	@Column
-//	private boolean basedOnOther;
+
+	@Column(nullable=false)
+	private Long careGiverId;
 	
 	MedicalCertificateEntity() {}
 	
@@ -95,30 +83,6 @@ public class MedicalCertificateEntity {
 	}
 
 	
-//	public boolean isActualSicknessProcess() {
-//		return actualSicknessProcess;
-//	}
-//
-//	public void setActualSicknessProcess(boolean actualSicknessProcess) {
-//		this.actualSicknessProcess = actualSicknessProcess;
-//	}
-//
-//	public boolean isExaminationResults() {
-//		return examinationResults;
-//	}
-//
-//	public void setExaminationResults(boolean examinationResults) {
-//		this.examinationResults = examinationResults;
-//	}
-//
-//	public boolean isLimitedActivity() {
-//		return limitedActivity;
-//	}
-//
-//	public void setLimitedActivity(boolean limitedActivity) {
-//		this.limitedActivity = limitedActivity;
-//	}
-
 	public Boolean getBasedOnExamination() {
 		return basedOnExamination;
 	}
@@ -135,22 +99,6 @@ public class MedicalCertificateEntity {
 		this.basedOnTelephoneContact = basedOnTelephoneContact;
 	}
 
-//	public boolean isBasedOnJournal() {
-//		return basedOnJournal;
-//	}
-//
-//	public void setBasedOnJournal(boolean basedOnJournal) {
-//		this.basedOnJournal = basedOnJournal;
-//	}
-//
-//	public boolean isBasedOnOther() {
-//		return basedOnOther;
-//	}
-//
-//	public void setBasedOnOther(boolean basedOnOther) {
-//		this.basedOnOther = basedOnOther;
-//	}
-
 	public Long getDiagnosisId() {
 		return diagnosisId;
 	}
@@ -165,5 +113,13 @@ public class MedicalCertificateEntity {
 
 	public void setCareUnitId(Long careUnitId) {
 		this.careUnitId = careUnitId;
+	}
+
+	public Long getCareGiverId() {
+		return careUnitId;
+	}
+
+	public void setCareGiverId(Long careGiverId) {
+		this.careGiverId = careGiverId;
 	}
 }
