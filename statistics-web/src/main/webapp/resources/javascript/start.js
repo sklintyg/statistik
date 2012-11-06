a75ebe364af9aeb579f38b4a22de550fdb3b7329
Peter Larsson <advptr@gmail.com>
2012-11-06 18:11:03 +0100

    Header corrected

--- statistics-web/src/main/webapp/resources/javascript/start.js
+++ statistics-web/src/main/webapp/resources/javascript/start.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Callista Enterprise AB <info@callistaenterprise.se>
+ * Copyright (C) 2012 Inera AB (http://www.inera.se)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,4 +14,3 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
