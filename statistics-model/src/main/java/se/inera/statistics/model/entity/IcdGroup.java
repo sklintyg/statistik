3fa65222c727a8a94660f3b84fc602cb0f05cd53
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2012-11-16 13:40:51 +0100

    Use standard case on field name.

--- statistics-model/src/main/java/se/inera/statistics/model/entity/IcdGroup.java
+++ statistics-model/src/main/java/se/inera/statistics/model/entity/IcdGroup.java
@@ -26,7 +26,7 @@ public class IcdGroup {
 	
 	private String chapter;
 	
-	private String Description;
+	private String description;
 	
 	IcdGroup(){
 	}
@@ -56,10 +56,10 @@ public class IcdGroup {
 	}
 
 	public String getDescription() {
-		return Description;
+		return description;
 	}
 
 	public void setDescription(String description) {
-		Description = description;
+		this.description = description;
 	}
 }
