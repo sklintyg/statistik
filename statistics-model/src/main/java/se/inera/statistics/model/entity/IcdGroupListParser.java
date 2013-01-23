38886c98d148e93e9ee2033f58f3dbbd4efc54f4
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-01-23 15:11:29 +0100

    Made utility class final.

--- statistics-model/src/main/java/se/inera/statistics/model/entity/IcdGroupListParser.java
+++ statistics-model/src/main/java/se/inera/statistics/model/entity/IcdGroupListParser.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class IcdGroupListParser {
+public final class IcdGroupListParser {
 
     private IcdGroupListParser() {
         // prevent instantiation
