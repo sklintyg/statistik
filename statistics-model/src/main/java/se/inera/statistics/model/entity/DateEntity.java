b00f30348965c9c52feb84c299a5585fb4ad7fd3
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-01-23 08:41:51 +0100

    Removed unused @Ignore directives. Reformat.

--- statistics-model/src/main/java/se/inera/statistics/model/entity/DateEntity.java
+++ statistics-model/src/main/java/se/inera/statistics/model/entity/DateEntity.java
@@ -29,120 +29,104 @@ import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(uniqueConstraints=@UniqueConstraint(columnNames={"calendarDate"}))
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "calendarDate" }))
 public class DateEntity {
-	DateEntity(){
-	}
-	
-	@Id
-	private long id;
+    DateEntity() {
+    }
 
-	@Column(nullable=false)
-	@Temporal(TemporalType.DATE)
-	private Date calendarDate;
-	
-	@SuppressWarnings("unused")
-	@Column(nullable=false)
-	private int calendarYear;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private int calendarMonth;
-	
-//	@SuppressWarnings("unused")
-	@Column
-	private String monthName;
-	
-	@SuppressWarnings("unused")
-	@Column(nullable=false)
-	private int calendarDay;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private int dayOfYear;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String weekDay;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private int calendarWeek;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String formattedDate;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String quartal;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String yearQuartal;
-	
-//	@SuppressWarnings("unused")
-	@Column
-	private String yearMonth;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String yearCalendarWeek;
-	
-	@SuppressWarnings("unused")
-	@Column
-	private String weekend;
-	
-	@SuppressWarnings("unused")
-	@Column
-	@Temporal(TemporalType.DATE)
-	private Date currentWeekStart;
+    @Id
+    private long id;
 
-	@SuppressWarnings("unused")
-	@Column
-	@Temporal(TemporalType.DATE)
-	private Date currentWeekEnd;
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date calendarDate;
 
-//	@SuppressWarnings("unused")
-	@Column
-	@Temporal(TemporalType.DATE)
-	private Date monthStart;
-	
-//	@SuppressWarnings("unused")
-	@Column
-	@Temporal(TemporalType.DATE)
-	private Date monthEnd;
+    @Column(nullable = false)
+    private int calendarYear;
 
-	public long getId() {
-		return id;
-	}
+    @Column
+    private int calendarMonth;
 
-	public void setId(long id) {
-		this.id = id;
-		
-	}
+    // @SuppressWarnings("unused")
+    @Column
+    private String monthName;
 
-	public Date getCalendarDate() {
-		return calendarDate;
-	}
+    @Column(nullable = false)
+    private int calendarDay;
 
-	public void setCalendarDate(Date calendarDate) {
-		this.calendarDate = calendarDate;
-	}
+    @Column
+    private int dayOfYear;
 
-	public Date getMonthStart() {
-		return monthStart;
-	}
+    @Column
+    private String weekDay;
 
-	public Date getMonthEnd() {
-		return monthEnd;
-	}
+    @Column
+    private int calendarWeek;
 
-	public String getMonthName() {
-		return monthName;
-	}
+    @Column
+    private String formattedDate;
 
-	public String getYearMonth() {
-		return yearMonth;
-	}
+    @Column
+    private String quartal;
+
+    @Column
+    private String yearQuartal;
+
+    @Column
+    private String yearMonth;
+
+    @Column
+    private String yearCalendarWeek;
+
+    @Column
+    private String weekend;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date currentWeekStart;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date currentWeekEnd;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date monthStart;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date monthEnd;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+
+    }
+
+    public Date getCalendarDate() {
+        return calendarDate;
+    }
+
+    public void setCalendarDate(Date calendarDate) {
+        this.calendarDate = calendarDate;
+    }
+
+    public Date getMonthStart() {
+        return monthStart;
+    }
+
+    public Date getMonthEnd() {
+        return monthEnd;
+    }
+
+    public String getMonthName() {
+        return monthName;
+    }
+
+    public String getYearMonth() {
+        return yearMonth;
+    }
 }
