67d719b1dbd6d55d978fa6d1a0a2115de5b20a88
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-02-01 10:37:50 +0100

    Make utility class final.

--- statistics-core/src/main/java/se/inera/statistics/core/spi/impl/QueryUtil.java
+++ statistics-core/src/main/java/se/inera/statistics/core/spi/impl/QueryUtil.java
@@ -1,10 +1,14 @@
 package se.inera.statistics.core.spi.impl;
 
-public class QueryUtil {
+public final class QueryUtil {
 
     public static final String FEMALE = "Female";
 
     public static final String MALE = "Male";
+
+    private QueryUtil() {
+        // Hide constructor for utility class.
+    }
 
     public static boolean isAllSelected(String disability) {
         return "all".equals(disability);
