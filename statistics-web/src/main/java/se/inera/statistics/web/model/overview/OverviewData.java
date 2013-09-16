37baa9b003a72207da1d40394794ebc949077dd0
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-09-16 10:25:02 +0200

    Made model classes immutable.

--- statistics-web/src/main/java/se/inera/statistics/web/model/overview/OverviewData.java
+++ statistics-web/src/main/java/se/inera/statistics/web/model/overview/OverviewData.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 public class OverviewData {
 
-    private NumberOfCasesPerMonthOverview casesPerMonth;
-    private List<DonutChartData> diagnosisGroups;
+    private final NumberOfCasesPerMonthOverview casesPerMonth;
+    private final List<DonutChartData> diagnosisGroups;
 
     public OverviewData(NumberOfCasesPerMonthOverview casesPerMonth, List<DonutChartData> diagnosisGroups) {
-        super();
         this.casesPerMonth = casesPerMonth;
         this.diagnosisGroups = diagnosisGroups;
     }
@@ -17,16 +16,7 @@ public class OverviewData {
         return casesPerMonth;
     }
 
-    public void setCasesPerMonth(NumberOfCasesPerMonthOverview casesPerMonth) {
-        this.casesPerMonth = casesPerMonth;
-    }
-
     public List<DonutChartData> getDiagnosisGroups() {
         return diagnosisGroups;
     }
-
-    public void setDiagnosisGroups(List<DonutChartData> diagnosisGroups) {
-        this.diagnosisGroups = diagnosisGroups;
-    }
-
 }
