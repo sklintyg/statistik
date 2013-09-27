c85f2a6bbb290544765d45000e0c4f59e4896d7b
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-09-27 11:23:45 +0200

    Added iteration for new months in SjukfallPerKonListener.

---
+++ statistics-service/src/test/java/se/inera/statistics/service/report/listener/SjukfallPerKonListenerTest.java
@@ -0,0 +1,56 @@
+package se.inera.statistics.service.report.listener;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.hibernate.ejb.criteria.expression.SearchedCaseExpression.WhenClause;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import se.inera.statistics.service.sjukfall.SjukfallInfo;
+
+public class SjukfallPerKonListenerTest {
+
+    private SjukfallPerKonListener listener = Mockito.spy(new SjukfallPerKonListener());
+
+    @Test
+    public void noPrevousEndReturnsNewStartMonth() {
+        LocalDate startMonth = new LocalDate("2013-08-05");
+        LocalDate firstDateMonth = SjukfallPerKonListener.getFirstDateMonth(null, startMonth);
+        assertEquals(startMonth.withDayOfMonth(1), firstDateMonth);
+    }
+
+    @Test
+    public void prevousEndBeforeNewMonthReturnsNewStartMonth() {
+        LocalDate previousEnd = new LocalDate("2013-07-21");
+        LocalDate startMonth = new LocalDate("2013-08-05");
+        LocalDate firstDateMonth = SjukfallPerKonListener.getFirstDateMonth(previousEnd, startMonth);
+        assertEquals(startMonth.withDayOfMonth(1), firstDateMonth);
+    }
+
+    @Test
+    public void prevousEndSameAsNewMonthReturnsNextMonth() {
+        LocalDate previousEnd = new LocalDate("2013-08-02");
+        LocalDate startMonth = new LocalDate("2013-08-05");
+        LocalDate firstDateMonth = SjukfallPerKonListener.getFirstDateMonth(previousEnd, startMonth);
+        assertEquals(startMonth.withDayOfMonth(1).plusMonths(1), firstDateMonth);
+    }
+    
+    @Test
+    public void acceptWithNoPreviousStartsCallingAtStartMonth() {
+        ArgumentCaptor<LocalDate> capture = ArgumentCaptor.forClass(LocalDate.class);
+        doNothing().when(listener).accept(capture.capture(), any(SjukfallInfo.class), any(JsonNode.class), any(JsonNode.class));
+
+        LocalDate firstDate = new LocalDate("2013-01-01");
+        LocalDate lastDate = new LocalDate("2013-01-01");
+        listener.loopOverPeriod(null, null, null, firstDate, lastDate);
+
+        assertNull(capture.getValue());
+    }
+
+}
