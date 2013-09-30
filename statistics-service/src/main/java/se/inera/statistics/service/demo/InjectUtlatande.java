62fdf6047288e455d05d9de4d222a64525275894
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-09-30 17:00:38 +0200

    Cleanup.

--- statistics-service/src/main/java/se/inera/statistics/service/demo/InjectUtlatande.java
+++ statistics-service/src/main/java/se/inera/statistics/service/demo/InjectUtlatande.java
@@ -1,34 +1,40 @@
 package se.inera.statistics.service.demo;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.joda.time.LocalDate;
-import org.joda.time.LocalTime;
-import org.joda.time.Period;
-import org.joda.time.ReadablePeriod;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+
 import se.inera.statistics.service.helper.JSONParser;
 
-import javax.annotation.PostConstruct;
-import javax.jms.*;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.Math.random;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class InjectUtlatande {
     private static final Logger LOG = LoggerFactory.getLogger(InjectUtlatande.class);
 
-    JmsTemplate jmsTemplate;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static final LocalDate BASE = new LocalDate("2012-03-01");
+
+    private JmsTemplate jmsTemplate;
     private ActiveMQQueue destination;
 
     public void setup() {
@@ -60,7 +66,6 @@ public class InjectUtlatande {
 
         for (String id : personNummers) {
             JsonNode newPermutation = permutate(intygTree, id);
-
             simpleSend(newPermutation.toString(), "C" + id);
         }
     }
@@ -68,25 +73,23 @@ public class InjectUtlatande {
     public static JsonNode permutate(JsonNode intyg, String id) {
         ObjectNode newPermutation = intyg.deepCopy();
         ObjectNode patientIdNode = (ObjectNode) newPermutation.path("patient").path("id");
-        patientIdNode.remove("extension");
         patientIdNode.put("extension", id);
 
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-
-        LocalDate start = formatter.parseLocalDate("2012-03-01");
-        start = start.plusMonths((int) (Math.random() * 19)).plusDays((int) (Math.random() * 30));
+        // CHECKSTYLE:OFF MagicNumber
+        LocalDate start = BASE.plusMonths((int) (Math.random() * 19)).plusDays((int) (Math.random() * 30));
         LocalDate end = start.plusDays((int) (Math.random() * 30 + 7));
+        // CHECKSTYLE:ON MagicNumber
+
         newPermutation.remove("validFromDate");
         newPermutation.remove("validToDate");
-        newPermutation.put("validFromDate", formatter.print(start));
-        newPermutation.put("validToDate", formatter.print(end));
+        newPermutation.put("validFromDate", FORMATTER.print(start));
+        newPermutation.put("validToDate", FORMATTER.print(end));
 
         LOG.info("New permutation" + newPermutation.toString());
         return newPermutation;
     }
 
     private void simpleSend(final String intyg, final String correlationId) {
-
         this.jmsTemplate.send(destination, new MessageCreator() {
             public Message createMessage(Session session) throws JMSException {
                 TextMessage message = session.createTextMessage(intyg);
@@ -113,7 +116,7 @@ public class InjectUtlatande {
     private List<String> readList(String path) {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(path), "utf8"));
-            List list = new ArrayList();
+            List<String> list = new ArrayList<>();
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 list.add(line);
             }
