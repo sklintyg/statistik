ca0814ae6c9a0431d02207a35dcb13814f1fdf8a
Pierre Sandboge <pierre.sandboge@callistaenterprise.se>
2013-10-02 10:33:07 +0200

    Mad CasesPerMonthKey jpa compliant.

--- statistics-service/src/main/java/se/inera/statistics/service/report/model/CasesPerMonthKey.java
+++ statistics-service/src/main/java/se/inera/statistics/service/report/model/CasesPerMonthKey.java
@@ -24,4 +24,39 @@ public class CasesPerMonthKey implements Serializable {
         return hsaId;
     }
 
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public void setHsaId(String hsaId) {
+        this.hsaId = hsaId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CasesPerMonthKey that = (CasesPerMonthKey) o;
+
+        if (hsaId != null ? !hsaId.equals(that.hsaId) : that.hsaId != null) {
+            return false;
+        }
+        if (period != null ? !period.equals(that.period) : that.period != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = period != null ? period.hashCode() : 0;
+        result = (2 * 2 * 2 * 2 * 2 - 1) * result + (hsaId != null ? hsaId.hashCode() : 0);
+        return result;
+    }
 }
