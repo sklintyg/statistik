006203a8277a18516a3d8cb02a4ca839eeab2311
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-10-07 23:44:11 +0200

    Cleanup imports.

--- statistics-service/src/main/java/se/inera/statistics/service/report/api/AgeGroups.java
+++ statistics-service/src/main/java/se/inera/statistics/service/report/api/AgeGroups.java
@@ -1,12 +1,8 @@
 package se.inera.statistics.service.report.api;
 
 import org.joda.time.LocalDate;
-import se.inera.statistics.service.report.model.AgeGroupsResponse;
-import se.inera.statistics.service.report.model.AgeGroupsRow;
-import se.inera.statistics.service.report.model.CasesPerMonthRow;
-import se.inera.statistics.service.report.model.Sex;
 
-import java.util.List;
+import se.inera.statistics.service.report.model.AgeGroupsResponse;
 
 public interface AgeGroups {
 
