53d65f749424d3699bf54e82ca69fdd6b03d1c36
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2013-10-11 15:34:44 +0200

    Cleaned up code by refactoring diagnosis groups collection

--- statistics-web/src/main/java/se/inera/statistics/web/service/DiagnosisGroupsConverter.java
+++ statistics-web/src/main/java/se/inera/statistics/web/service/DiagnosisGroupsConverter.java
@@ -1,9 +1,12 @@
 package se.inera.statistics.web.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -22,15 +25,26 @@ import se.inera.statistics.web.model.TableHeader;
 
 public class DiagnosisGroupsConverter {
 
-    private static final List<String> DIAGNOSIS_CHART_GROUPS = Arrays.asList(
-            "Somatiska sjukdomar (A00-E90, G00-L99, N00-N99)",
-            "Psykiska sjukdomar (F00-F99)",
-            "Muskuloskeletala sjukdomar (M00-M99)",
-            "Graviditet och förlossning (O00-O99)",
-            "Övrigt (P00-P96, Q00-Q99, S00-Y98)",
-            "Symtomdiagnoser (R00-R99)",
-            "Faktorer av betydelse för hälsotillståndet och för kontakter med hälso- och sjukvården (Z00-Z99)");
+    private static final LinkedHashMap<String, List<String>> DIAGNOSIS_CHART_GROUPS = createDiagnosisGroupsMap(); 
+    private static final String OVRIGT_CHART_GROUP = "Övrigt (P00-P96, Q00-Q99, S00-Y98)";
 
+    private static LinkedHashMap<String, List<String>> createDiagnosisGroupsMap(){
+        final LinkedHashMap<String, List<String>> diagnosisGroups = new LinkedHashMap<>();
+        diagnosisGroups.put("Somatiska sjukdomar (A00-E90, G00-L99, N00-N99)", Arrays.asList("A00-B99", "C00-D48", "D50-D89", "E00-E90", "G00-G99", "H00-H59",
+                "H00-H59", "H60-H95", "I00-I99", "J00-J99", "K00-K93", "L00-L99", "N00-N99"));
+        diagnosisGroups.put("Psykiska sjukdomar (F00-F99)", Arrays.asList("F00-F99"));
+        diagnosisGroups.put("Muskuloskeletala sjukdomar (M00-M99)", Arrays.asList("M00-M99"));
+        diagnosisGroups.put("Graviditet och förlossning (O00-O99)", Arrays.asList("O00-O99"));
+        diagnosisGroups.put(OVRIGT_CHART_GROUP, Arrays.asList("P00-P96", "Q00-Q99", "S00-T98", "U00-U99", "V01-Y98"));
+        diagnosisGroups.put("Symtomdiagnoser (R00-R99)", Arrays.asList("R00-R99"));
+        diagnosisGroups.put("Faktorer av betydelse för hälsotillståndet och för kontakter med hälso- och sjukvården (Z00-Z99)", Arrays.asList("Z00-Z99"));
+        return diagnosisGroups;
+    }
+
+    private static List<String> getDiagnosisChartGroupsAsList(){
+        return new ArrayList<>(DIAGNOSIS_CHART_GROUPS.keySet());
+    }
+    
     DualSexStatisticsData convert(DiagnosisGroupResponse diagnosisGroups) {
         TableData tableData = convertTable(diagnosisGroups);
         ChartData maleChart = convertChart(diagnosisGroups, Sex.Male);
@@ -55,13 +69,13 @@ public class DiagnosisGroupsConverter {
 
             @Override
             public int compare(String o1, String o2) {
-                return DIAGNOSIS_CHART_GROUPS.indexOf(o1) - DIAGNOSIS_CHART_GROUPS.indexOf(o2);
+                return getDiagnosisChartGroupsAsList().indexOf(o1) - getDiagnosisChartGroupsAsList().indexOf(o2);
             }
 
         });
         List<List<Integer>> values = new ArrayList<List<Integer>>(allGroups.values());
         int listSize = values.isEmpty() ? 0 : values.get(0).toArray().length;
-        for(String groupName : DIAGNOSIS_CHART_GROUPS) {
+        for (String groupName : getDiagnosisChartGroupsAsList()) {
             mergedGroups.put(groupName, createZeroFilledList(listSize));
         }
         for (Entry<String, List<Integer>> entry : allGroups.entrySet()) {
@@ -72,7 +86,7 @@ public class DiagnosisGroupsConverter {
 
     private List<Integer> createZeroFilledList(int listSize) {
         ArrayList<Integer> listOfZeros = new ArrayList<>();
-        for( int i = 0; i < listSize; i++) {
+        for (int i = 0; i < listSize; i++) {
             listOfZeros.add(0);
         }
         return listOfZeros;
@@ -107,35 +121,16 @@ public class DiagnosisGroupsConverter {
     }
 
     private String getMergedChartGroupName(String groupId) {
-        List<String> g1 = Arrays.asList("A00-B99", "C00-D48", "D50-D89", "E00-E90", "G00-G99", "H00-H59", "H00-H59", "H60-H95", "I00-I99",
-                "J00-J99", "K00-K93", "L00-L99", "N00-N99");
-        List<String> g2 = Arrays.asList("F00-F99");
-        List<String> g3 = Arrays.asList("M00-M99");
-        List<String> g4 = Arrays.asList("O00-O99");
-        List<String> g5 = Arrays.asList("P00-P96", "Q00-Q99", "S00-T98", "U00-U99", "V01-Y98");
-        List<String> g6 = Arrays.asList("R00-R99");
-        List<String> g7 = Arrays.asList("Z00-Z99");
-        if (g1.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(0);
-        } else if (g2.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(1);
-        } else if (g3.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(2);
-        } else if (g4.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(3);
-        } else if (g5.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(4);
-        } else if (g6.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(5);
-        } else if (g7.contains(groupId)) {
-            return DIAGNOSIS_CHART_GROUPS.get(6);
-        } else {
-            // Unknown groups should never occur, but if it do than add it to
-            // the Ovrigt-group (or fail in development)
-            //TODO Add error logging
-            assert false;
-            return DIAGNOSIS_CHART_GROUPS.get(4);
+        for (Entry<String, List<String>> entry : DIAGNOSIS_CHART_GROUPS.entrySet()) {
+            if (entry.getValue().contains(groupId)){
+                return entry.getKey();
+            }
         }
+        // Unknown groups should never occur, but if it do than add it to
+        // the Ovrigt-group (or fail in development)
+        //TODO Add error logging
+        assert false;
+        return OVRIGT_CHART_GROUP;
     }
 
     static TableData convertTable(DiagnosisGroupResponse resp) {
