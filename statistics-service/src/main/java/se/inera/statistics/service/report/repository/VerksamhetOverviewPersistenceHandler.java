e2857a1773ad56e0ecca93cec1ae11f6f34b0f69
Pierre Sandboge <pierre.sandboge@callistaenterprise.se>
2013-10-16 13:23:38 +0200

    Added verksamhet overview diagnosis groups test.

--- statistics-service/src/main/java/se/inera/statistics/service/report/repository/VerksamhetOverviewPersistenceHandler.java
+++ statistics-service/src/main/java/se/inera/statistics/service/report/repository/VerksamhetOverviewPersistenceHandler.java
@@ -1,35 +1,57 @@
 package se.inera.statistics.service.report.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.transaction.annotation.Transactional;
 import se.inera.statistics.service.report.api.VerksamhetOverview;
-import se.inera.statistics.service.report.model.DiagnosisGroupResponse;
 import se.inera.statistics.service.report.model.OverviewChartRowExtended;
 import se.inera.statistics.service.report.model.Range;
 import se.inera.statistics.service.report.model.VerksamhetOverviewResponse;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VerksamhetOverviewPersistenceHandler implements VerksamhetOverview {
+    private DateTimeFormatter inputFormatter = DateTimeFormat.forPattern("yyyy-MM");
+
     @PersistenceContext(unitName = "IneraStatisticsLog")
     private EntityManager manager;
-
-    @Autowired
-    DiagnosgroupPersistenceHandler diagnosgroupPersistenceHandler;
 
     @Override
     public VerksamhetOverviewResponse getOverview(String verksamhetId, Range range) {
 
 
-        ArrayList<OverviewChartRowExtended> diagnosisGroups = getDiagnosisGroups(verksamhetId, range);
+        List<OverviewChartRowExtended> diagnosisGroups = getDiagnosisGroups(verksamhetId, range);
+        List<OverviewChartRowExtended> ageGroups = getAgeGroups(verksamhetId, range);
 
-        return new VerksamhetOverviewResponse(0, 0, 0, diagnosisGroups, null, null, null, 0, 0, null);
+        return new VerksamhetOverviewResponse(0, 0, 0, diagnosisGroups, ageGroups, null, null, 0, 0, null);
     }
 
     @Transactional
-    private ArrayList<OverviewChartRowExtended> getDiagnosisGroups(String verksamhetId, Range range) {
+    @SuppressWarnings("unchecked")
+    private List<OverviewChartRowExtended> getDiagnosisGroups(String verksamhetId, Range range) {
+        Query query = manager.createQuery("SELECT c.diagnosisGroupKey.diagnosgrupp, sum(c.female) + sum(c.male), c.diagnosisGroupKey.diagnosgrupp  FROM DiagnosisGroupData c WHERE c.diagnosisGroupKey.hsaId = :hsaId AND c.diagnosisGroupKey.period >= :from AND c.diagnosisGroupKey.period <= :to  GROUP BY c.diagnosisGroupKey.diagnosgrupp");
+        query.setParameter("hsaId", verksamhetId);
+        query.setParameter("from", inputFormatter.print(range.getFrom()));
+        query.setParameter("to", inputFormatter.print(range.getTo()));
+
+        List<Object[]> queryResult;
+        queryResult = (List<Object[]>) query.getResultList();
+
+        ArrayList<OverviewChartRowExtended> result = new ArrayList<>();
+        for (Object[] row : queryResult) {
+            result.add(new OverviewChartRowExtended((String) row[0], ((Long) row[1]).intValue(), 0));
+        }
+
+        return result;
+    }
+
+    @Transactional
+    private List<OverviewChartRowExtended> getAgeGroups(String verksamhetId, Range range) {
         return null;
     }
+
 }
--- statistics-service/src/test/java/se/inera/statistics/service/report/repository/VerksamhetOverviewPersistanceHandlerTest.java
+++ statistics-service/src/test/java/se/inera/statistics/service/report/repository/VerksamhetOverviewPersistanceHandlerTest.java
@@ -1,5 +1,45 @@
 package se.inera.statistics.service.report.repository;
 
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import se.inera.statistics.service.report.api.DiagnosisGroups;
+import se.inera.statistics.service.report.model.Range;
+import se.inera.statistics.service.report.model.Sex;
+import se.inera.statistics.service.report.model.VerksamhetOverviewResponse;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:process-log-impl-test.xml" })
+@Transactional
+@DirtiesContext
 public class VerksamhetOverviewPersistanceHandlerTest extends VerksamhetOverviewPersistenceHandler {
 
+    @Autowired
+    private DiagnosisGroups diagnosgroupPersistenceHandler;
+
+    @Before
+    public void init() {
+        diagnosgroupPersistenceHandler.count("id1", "2013-09", "g1", Sex.Female);
+        diagnosgroupPersistenceHandler.count("id1", "2013-09", "g1", Sex.Female);
+        diagnosgroupPersistenceHandler.count("id3", "2013-09", "g1", Sex.Male);
+        diagnosgroupPersistenceHandler.count("id1", "2012-09", "g1", Sex.Male);
+    }
+
+    @Test
+    public void getDiagnosisGroupsTest() {
+        LocalDate from = new LocalDate(2013,8,1);
+        LocalDate to = new LocalDate(2013,10,1);
+        Range range = new Range(from, to);
+
+        VerksamhetOverviewResponse result = this.getOverview("id1", range);
+
+        Assert.assertEquals(2, result.getDiagnosisGroups().get(0).getQuantity());
+    }
 }
