ad5ac55ef619fcad911eb6101b4cc12f1b0a45d3
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2013-10-17 11:06:14 +0200

    Refactored to a common model for data used by the some of the details views

--- statistics-web/src/main/java/se/inera/statistics/web/model/CasesPerCountyData.java
+++ statistics-web/src/main/java/se/inera/statistics/web/model/CasesPerCountyData.java
@@ -1,26 +1,9 @@
 package se.inera.statistics.web.model;
 
-public class CasesPerCountyData {
-
-    private final TableData tableData;
-    private final ChartData chartData;
-    private final int monthsIncluded;
+public class CasesPerCountyData extends SimpleDetailsData {
 
     public CasesPerCountyData(TableData tableData, ChartData chartData, int monthsIncluded) {
-        this.tableData = tableData;
-        this.chartData = chartData;
-        this.monthsIncluded = monthsIncluded;
+        super(tableData, chartData, monthsIncluded);
     }
 
-    public TableData getTableData() {
-        return tableData;
-    }
-
-    public ChartData getChartData() {
-        return chartData;
-    }
-
-    public int getMonthsIncluded() {
-        return monthsIncluded;
-    }
 }
--- statistics-web/src/main/java/se/inera/statistics/web/model/SickLeaveLengthData.java
+++ statistics-web/src/main/java/se/inera/statistics/web/model/SickLeaveLengthData.java
@@ -1,26 +1,9 @@
 package se.inera.statistics.web.model;
 
-public class SickLeaveLengthData {
-
-    private final TableData tableData;
-    private final ChartData chartData;
-    private final int monthsIncluded;
+public class SickLeaveLengthData extends SimpleDetailsData {
 
     public SickLeaveLengthData(TableData tableData, ChartData chartData, int monthsIncluded) {
-        this.tableData = tableData;
-        this.chartData = chartData;
-        this.monthsIncluded = monthsIncluded;
+        super(tableData, chartData, monthsIncluded);
     }
 
-    public TableData getTableData() {
-        return tableData;
-    }
-
-    public ChartData getChartData() {
-        return chartData;
-    }
-
-    public int getMonthsIncluded() {
-        return monthsIncluded;
-    }
 }
