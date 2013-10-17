da02e4de5e37f7a95bd90b3f9c5950babadea2d1
Pierre Sandboge <pierre.sandboge@callistaenterprise.se>
2013-10-17 10:32:41 +0200

    Added mysql support. Mysql is default, you must use mvn jetty:run -Dspring.profiles.active=embedded to use H2 from now on.

--- statistics-service/src/main/java/se/inera/statistics/service/processlog/Event.java
+++ statistics-service/src/main/java/se/inera/statistics/service/processlog/Event.java
@@ -10,7 +10,7 @@ import javax.persistence.MappedSuperclass;
 public class Event {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Enumerated
