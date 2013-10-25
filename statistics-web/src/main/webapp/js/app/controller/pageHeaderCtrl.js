23b3a37d0359e8d715c47975d2d198df192df724
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2013-10-25 14:56:30 +0200

    Fixed nonstandard way to format dates in javascript

--- statistics-web/src/main/webapp/js/app/controller/pageHeaderCtrl.js
+++ statistics-web/src/main/webapp/js/app/controller/pageHeaderCtrl.js
@@ -4,7 +4,12 @@
 
     $rootScope.$on('$routeChangeSuccess', function(angularEvent, next, current) {
         $scope.currentUrl = window.location.href;
-        $scope.currentTime = new Date().toLocaleFormat("%Y-%m-%d");
+
+        var d = new Date();
+        var curr_date = d.getDate();
+        var curr_month = d.getMonth() + 1; //Months are zero based
+        var curr_year = d.getFullYear();
+        $scope.currentTime = curr_year + "-" + curr_month + "-" + curr_date;
     });
 
  };
