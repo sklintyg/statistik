002df4c8570bc50fdc77a1d39b7d1f2732d19c57
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-02-19 10:07:44 +0100

    Make Credentials serailizable (needed by Token). Cleanup unused code.

--- statistics-web/src/main/java/se/inera/auth/FakeCredentials.java
+++ statistics-web/src/main/java/se/inera/auth/FakeCredentials.java
@@ -19,18 +19,18 @@
 
 package se.inera.auth;
 
+import java.io.Serializable;
+
 /**
  * @author andreaskaltenbach
  */
-public class FakeCredentials {
+@SuppressWarnings("serial")
+public class FakeCredentials implements Serializable {
 
-    private String hsaId;
-    private String fornamn;
-    private String efternamn;
-    private boolean lakare;
-
-    public FakeCredentials() {
-    }
+    private final String hsaId;
+    private final String fornamn;
+    private final String efternamn;
+    private final boolean lakare;
 
     public FakeCredentials(String hsaId, String fornamn, String efternamn, boolean lakare) {
         this.hsaId = hsaId;
@@ -53,22 +53,6 @@ public class FakeCredentials {
 
     public boolean isLakare() {
         return lakare;
-    }
-
-    public void setHsaId(String hsaId) {
-        this.hsaId = hsaId;
-    }
-
-    public void setFornamn(String fornamn) {
-        this.fornamn = fornamn;
-    }
-
-    public void setEfternamn(String efternamn) {
-        this.efternamn = efternamn;
-    }
-
-    public void setLakare(boolean lakare) {
-        this.lakare = lakare;
     }
 
     @Override
