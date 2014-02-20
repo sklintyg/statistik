e1136bf8ced1bfa964162e74ee819db45a4d1c1c
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-02-20 09:50:47 +0100

    Add back constructor and setters as needed by Json mapper.

--- statistics-web/src/main/java/se/inera/auth/FakeCredentials.java
+++ statistics-web/src/main/java/se/inera/auth/FakeCredentials.java
@@ -27,10 +27,13 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class FakeCredentials implements Serializable {
 
-    private final String hsaId;
-    private final String fornamn;
-    private final String efternamn;
-    private final boolean lakare;
+    private String hsaId;
+    private String fornamn;
+    private String efternamn;
+    private boolean lakare;
+
+    public FakeCredentials() {
+    }
 
     public FakeCredentials(String hsaId, String fornamn, String efternamn, boolean lakare) {
         this.hsaId = hsaId;
@@ -53,6 +56,22 @@ public class FakeCredentials implements Serializable {
 
     public boolean isLakare() {
         return lakare;
+    }
+
+    public void setHsaId(String hsaId) {
+        this.hsaId = hsaId;
+    }
+
+    public void setFornamn(String fornamn) {
+        this.fornamn = fornamn;
+    }
+
+    public void setEfternamn(String efternamn) {
+        this.efternamn = efternamn;
+    }
+
+    public void setLakare(boolean lakare) {
+        this.lakare = lakare;
     }
 
     @Override
