904921eebac998f2f86bce5ef16c323e5dcc1553
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-03-14 10:49:34 +0100

    STATISTIK-504 Added job which reloads facts from db. Use setting scheduler. Use scheduler.factReloadJob.cron. Separated long running tests into Integration tests.

--- statistics-service/src/test/java/se/inera/statistics/service/warehouse/WarehouseTest.java
+++ statistics-service/src/test/java/se/inera/statistics/service/warehouse/WarehouseTest.java
@@ -1,6 +1,7 @@
 package se.inera.statistics.service.warehouse;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.lang.management.GarbageCollectorMXBean;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import org.springframework.util.StopWatch;
 import se.inera.statistics.service.JSONSource;
 import se.inera.statistics.service.demo.LargeTestDataGenerator;
 import se.inera.statistics.service.helper.DocumentHelper;
@@ -39,9 +41,6 @@ public class WarehouseTest {
     @Autowired
     private Warehouse warehouse;
 
-    @Autowired 
-    private LargeTestDataGenerator dataGenerator;
-
     @Autowired
     private WidelineConverter widelineConverter;
 
@@ -55,72 +54,5 @@ public class WarehouseTest {
         factPopulator.accept(wideLine);
         Aisle aisle = warehouse.get("VardgivarId");
         assertEquals(1, aisle.getSize());
-    }
-
-    @Test
-    public void addingManyIntyg() throws InterruptedException {
-        System.err.println(warehouse);
-        showMem(); 
-        dataGenerator.publishUtlatanden();
-        showMem();
-        System.gc();
-        showMem();
-        System.err.println(warehouse);
-        final Aisle aisle = warehouse.get("vardgivare1");
-        //assertEquals(790, aisle.getSize());
-        ExecutorService pool = Executors.newFixedThreadPool(10);
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 100; i++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    long start = System.currentTimeMillis();
-                    measureSjukfall(aisle);
-                    long end = System.currentTimeMillis();
-                    System.err.println((end - start));
-                }
-            });
-        }
-        
-        pool.shutdown();
-        pool.awaitTermination(30, TimeUnit.SECONDS);
-        long end = System.currentTimeMillis();
-        System.err.println("Total time " + (end - start));
-        showMem();
-    }
-
-    @Test
-    public void exportManyIntyg() throws FileNotFoundException {
-        dataGenerator.publishUtlatanden();
-
-        String result = dataGenerator.exportUtlatanden();
-
-        //System.setOut(new PrintStream("intyg.txt"));
-        System.out.println(result);
-    }
-
-    private void measureSjukfall(Aisle aisle) {
-        Collection<Sjukfall> sjukfalls = SjukfallUtil.calculateSjukfall(aisle);
-        int totalDays = 0;
-        int realDays = 0;
-        int totalIntyg = 0;
-        for (Sjukfall sjukfall: sjukfalls) {
-            totalDays += (sjukfall.end - sjukfall.start);
-            realDays += sjukfall.realDays;
-            totalIntyg += sjukfall.intygCount;
-        }
-        System.err.format("Sjukfall %1$s Real days %2$s Total days %3$s intyg %4$s%n", sjukfalls.size(), realDays, totalDays, totalIntyg);
-    }
-
-    private void showMem() {
-        StringBuilder gc = new StringBuilder();
-        for (GarbageCollectorMXBean bean: ManagementFactory.getGarbageCollectorMXBeans()) {
-            gc.append(bean.getCollectionCount())
-            .append(" ").append(bean.getCollectionTime()).append(" ");
-        }
-        
-        Runtime r = Runtime.getRuntime();
-        long used = r.totalMemory() - r.freeMemory();
-        System.err.format("Memory total %1$s free %2$s used %3$s %4$s%n", r.totalMemory(), r.freeMemory(), used, gc.toString());
     }
 }
