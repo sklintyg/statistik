19b601f7fb5d9bd57d1bc5fbbf025c0c51c44a68
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-03-20 23:29:33 +0100

    STATISTIK-541 Save län per vårdgivare, not enhet.

--- statistics-service/src/main/java/se/inera/statistics/service/report/listener/SjukfallPerLanListener.java
+++ statistics-service/src/main/java/se/inera/statistics/service/report/listener/SjukfallPerLanListener.java
@@ -12,7 +12,7 @@ public class SjukfallPerLanListener extends RollingAbstractListener {
     private CasesPerCounty casesPerCounty;
 
     protected boolean accept(GenericHolder token, String period, RollingLength length) {
-        casesPerCounty.count(period, token.getEnhetId(), token.getLanId(), length, token.getKon());
+        casesPerCounty.count(period, token.getVardgivareId(), token.getLanId(), length, token.getKon());
         return false; // TODO: Caching
     }
 
