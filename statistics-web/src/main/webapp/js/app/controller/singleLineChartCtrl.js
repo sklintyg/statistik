186fdc680bfc6826a3d08f6fec58b5c1be9ed93f
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-08-14 13:09:07 +0200

    STATISTIK-565 Ändrade utrymme för grafvärden (y-axel) till 70 pixlar för att de inte skall klippas av när de blir > 5 siffror.

--- statistics-web/src/main/webapp/js/app/controller/singleLineChartCtrl.js
+++ statistics-web/src/main/webapp/js/app/controller/singleLineChartCtrl.js
@@ -46,7 +46,7 @@
 	var paintChart = function(chartCategories, chartSeries) {
         var chartOptions = ControllerCommons.getHighChartConfigBase(chartCategories, chartSeries);
         chartOptions.chart.type = 'line';
-        chartOptions.chart.marginLeft = 60;
+        chartOptions.chart.marginLeft = 70;
         chartOptions.chart.marginTop = 27;
         chartOptions.legend.enabled = $routeParams.printBw || $routeParams.print;
         chartOptions.xAxis.title.text = "Period";
