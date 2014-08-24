7be8b52dcb05d7922c6b217007d7bcdb269928e2
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2014-08-24 10:38:15 +0200

    Flyttade före detta entitet från db paketet.

--- statistics-service/src/main/java/se/inera/statistics/service/report/model/SjukfallslangdRow.java
+++ statistics-service/src/main/java/se/inera/statistics/service/report/model/SjukfallslangdRow.java
@@ -17,7 +17,7 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package se.inera.statistics.service.report.model.db;
+package se.inera.statistics.service.report.model;
 
 import se.inera.statistics.service.report.util.Verksamhet;
 
