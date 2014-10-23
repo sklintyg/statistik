07d970b125832a2b8b9765fc104d1d856c7e1569
Johan Lindholm <johan.lindholm@usify.se>
2014-10-23 09:04:41 +0200

    STATISTIK-697: Fixat ikonen som föll bort.

--- statistics-web/src/main/webapp/js/app/directives.js
+++ statistics-web/src/main/webapp/js/app/directives.js
@@ -142,7 +142,7 @@ angular.module('StatisticsApp').directive("filterButton", function () {
     	restrict: "E",
 	    template:
 	        '<button id="show-hide-filter-btn" type="button" class="btn btn-small pull-right" data-toggle="collapse" data-target="#statistics-filter-container" ng-click="filter.open = !filter.open">' +
-	        '<i class="glyphicon" ng-class="{glyphicon-chevron-up: !filter.open, glyphicon-chevron-down: filter.open}"></i> {{filter.open ? "Dölj filter" : "Visa filter"}}' +
+	        '<i class="glyphicon" ng-class="{glyphiconDownSign: !filter.open, glyphiconUpSign: filter.open}"></i> {{filter.open ? "Dölj filter" : "Visa filter"}}' +
 	        '</button>',
     };
 });
