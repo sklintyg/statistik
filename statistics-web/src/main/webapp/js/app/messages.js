44f9b0daf142d188c2293447a2ff3ceb559419a5
Johan Lindholm <johan.lindholm@usify.se>
2014-10-27 16:04:29 +0100

    Errorpages is now message keys..

--- statistics-web/src/main/webapp/js/app/messages.js
+++ statistics-web/src/main/webapp/js/app/messages.js
@@ -109,10 +109,11 @@ var stMessages = {
         'about.service': 'Allmänt om tjänsten',
         'about.cookies': '<h3>Om kakor (cookies)</h3><p>Så kallade kakor (cookies) används för att underlätta för besökaren på webbplatsen. En kaka är en textfil som lagras på din dator och som innehåller information. Denna webbplats använder så kallade sessionskakor. Sessionskakor lagras temporärt i din dators minne under tiden du är inne på en webbsida. Sessionskakor försvinner när du stänger din webbläsare. Ingen personlig information om dig sparas vid användning av sessionskakor.</p><p>Om du inte accepterar användandet av kakor kan du stänga av det via din webbläsares säkerhetsinställningar. Du kan även ställa in webbläsaren så att du får en varning varje gång webbplatsen försöker sätta en kaka på din dator.</p><p><strong>Observera!</strong> Om du stänger av kakor i din webbläsare kan du inte logga in i Webcert.</p><p>Allmän information om kakor (cookies) och lagen om elektronisk kommunikation finns på Post- och telestyrelsens webbplats.</p><p><a href="http://www.pts.se/sv/Bransch/Regler/Lagar/Lag-om-elektronisk-kommunikation/Cookies-kakor/" target="_blank">Mer om kakor (cookies) på Post- och telestyrelsens webbplats</a></p>',
 
-        //info messages
-        'info.nounsignedcertsfound': '<strong>Inga ej signerade intyg hittades.</strong>',
-
         //error messages
+        'error.data-for-vald-rapport-kan-ej-visas': 'Data för vald rapport går ej att visa',
+        'error.data-for-vald-rapport-kan-ej-visas.information': 'Rapporten kan ej visas. Om felet kvarstår kan du kontakta <a ng-href="#/om/kontakt">support</a> eller gå till startsidan och försöka igen.',
+        'error.sidan-kan-inte-visas': 'Sidan du söker går ej att hitta (404)',
+        'error.sidan-kan-inte-visas.information': 'Kontrollera eventuella stavfel eller gå till <a ng-href="#/">startsidan</a>. Du kan även kontakta <a ng-href="#/om/kontakt">support</a>.',
         'error.unsignedcerts.couldnotbeloaded': '<strong>Kunde inte hämta ej signerade intyg.</strong>'
     },
     'en': {
