33c3829c8ae00bfead7c1118269656e5ad8a60c1
Pierre Sandboge <pierre.sandboge@standin.se>
2014-10-30 15:03:21 +0100

    STATISTIK-670 Uppdaterde groovy, anonymisera även hsa-läkarid, hoppa över sjukfallsanonymisering om tabellen saknas (borttagen i o m statistik 2.0)

--- tools/inera-anonymisera-statistik/src/main/java/se/inera/statistics/tools/AnonymiseraStatistikDatabas.groovy
+++ tools/inera-anonymisera-statistik/src/main/java/se/inera/statistics/tools/AnonymiseraStatistikDatabas.groovy
@@ -1,5 +1,8 @@
 package se.inera.statistics.tools
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 import groovy.sql.Sql
 import groovyx.gpars.GParsPool
 
@@ -34,16 +37,23 @@ class AnonymiseraStatistikDatabas {
                                 initialSize: numberOfThreads, maxTotal: numberOfThreads)
         def bootstrapSql = new Sql(dataSource)
         def certificateIds = bootstrapSql.rows("select correlationId from intyghandelse")
-        def personIds = bootstrapSql.rows("select id, personId from sjukfall")
-        bootstrapSql.close()
         println "${certificateIds.size()} certificates found to anonymize"
-        println "${personIds.size()} sjukfall found to anonymize"
+        def personIds
+        try {
+            personIds = bootstrapSql.rows("select id, personId from sjukfall")
+            println "${personIds.size()} sjukfall found to anonymize"
+        } catch (MySQLSyntaxErrorException e) {
+            personIds = null
+        }
+        def hsaIds = bootstrapSql.rows("select id from hsa")
+        println "${hsaIds.size()} hsa found to anonymize"
+        bootstrapSql.close()
         final AtomicInteger count = new AtomicInteger(0)
         final AtomicInteger errorCount = new AtomicInteger(0)
         def output
         GParsPool.withPool(numberOfThreads) {
             output = certificateIds.collectParallel {
-                StringBuffer result = new StringBuffer() 
+                StringBuffer result = new StringBuffer()
                 def id = it.correlationId
                 Sql sql = new Sql(dataSource)
                 try {
@@ -72,24 +82,61 @@ class AnonymiseraStatistikDatabas {
             if (line) println line
         }
         println "Done! ${count} certificates anonymized with ${errorCount} errors in ${(int)((end-start) / 1000)} seconds"
-        count.set(0)
-        errorCount.set(0)
-        start = System.currentTimeMillis()
+        println "Proceeding to sjukfall"
+        if (personIds != null) {
+            count.set(0)
+            errorCount.set(0)
+            start = System.currentTimeMillis()
+            GParsPool.withPool(numberOfThreads) {
+                output = personIds.collectParallel {p ->
+                    StringBuffer result = new StringBuffer()
+                    def anonymPersonId = anonymiseraPersonId.anonymisera(p.personId)
+                    def id = p.id
+                    Sql sql = new Sql(dataSource)
+                    try {
+                        sql.executeUpdate('update sjukfall set personId = :personId where id = :id',
+                                [personId: anonymPersonId, id: id])
+                        int current = count.addAndGet(1)
+                        if (current % 10000 == 0) {
+                            println "${current} sjukfall anonymized in ${(int)((System.currentTimeMillis()-start) / 1000)} seconds"
+                        }
+                    } catch (Throwable t) {
+                        t.printStackTrace()
+                        result << "Anonymizing ${id} failed: ${t}"
+                        errorCount.incrementAndGet()
+                    } finally {
+                        sql.close()
+                    }
+                    result.toString()
+                }
+            }
+            end = System.currentTimeMillis()
+            output.each {line ->
+                if (line) println line
+            }
+            println "Done! ${count} certificates anonymized with ${errorCount} errors in ${(int)((end-start) / 1000)} seconds"
+        } else {
+            println "Sjukfall table missing, anonymization not necessary"
+        }
+        println "Proceeding to hsa"
         GParsPool.withPool(numberOfThreads) {
-            output = personIds.collectParallel {p ->
+            output = hsaIds.collectParallel {
                 StringBuffer result = new StringBuffer()
-                def anonymPersonId = anonymiseraPersonId.anonymisera(p.personId)
-                def id = p.id
                 Sql sql = new Sql(dataSource)
+                def id = it.id
                 try {
-                    sql.executeUpdate('update sjukfall set personId = :personId where id = :id',
-                            [personId: anonymPersonId, id: id])
+                    def intyg = sql.firstRow( 'select data from hsa where id = :id' , [id : id])
+                    String jsonDoc = intyg.data
+                    String anonymiseradJson = anonymizeHsaJson(jsonDoc, anonymiseraHsaId, id)
+                    sql.executeUpdate('update hsa set data = :document where id = :id',
+                            [document: anonymiseradJson, id: id])
                     int current = count.addAndGet(1)
                     if (current % 10000 == 0) {
-                        println "${current} sjukfall anonymized in ${(int)((System.currentTimeMillis()-start) / 1000)} seconds"
+                        println "${current} hsa:s anonymized in ${(int)((System.currentTimeMillis()-start) / 1000)} seconds"
                     }
                 } catch (Throwable t) {
                     t.printStackTrace()
+
                     result << "Anonymizing ${id} failed: ${t}"
                     errorCount.incrementAndGet()
                 } finally {
@@ -102,8 +149,18 @@ class AnonymiseraStatistikDatabas {
         output.each {line ->
             if (line) println line
         }
-        println "Done! ${count} certificates anonymized with ${errorCount} errors in ${(int)((end-start) / 1000)} seconds"
+        println "Done! ${count} hsa:s anonymized with ${errorCount} errors in ${(int)((end-start) / 1000)} seconds"
     }
+
+    static def anonymizeHsaJson(def s, def anonymiseraHsaId, def idx) {
+        def hsa = new JsonSlurper().parseText(s)
+        if (hsa.personal) {
+            hsa.personal.id = anonymiseraHsaId.anonymisera(hsa.personal.id)
+        }
+        JsonBuilder builder = new JsonBuilder(hsa)
+        return builder.toString()
+    }
+
 }
     
 
