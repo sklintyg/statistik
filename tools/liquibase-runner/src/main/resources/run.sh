fc53bb6737ae6efabee5cf7d144fb51b40a4736c
Pierre Sandboge <pierre.sandboge@standin.se>
2014-11-11 10:55:22 +0100

    Liquibase run.sh uppdaterad med rätt jar-filsnamn

--- tools/liquibase-runner/src/main/resources/run.sh
+++ tools/liquibase-runner/src/main/resources/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar target/liquibase-runner-for-statistics-1.1-SNAPSHOT-jar-with-dependencies.jar --changeLogFile="changelog/changelog.xml" --contexts=none update
+java -jar target/statistik-liquibase-runner-1.1-SNAPSHOT-jar-with-dependencies.jar --changeLogFile="changelog/changelog.xml" --contexts=none update
