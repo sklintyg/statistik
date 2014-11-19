ad146c08ddcc6e512a29e16d7cbd722dfa2f8b6f
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2014-11-19 16:41:09 +0100

    STATISTIK-751 Har lagt till rest service för att repopulera cachen för nationell data

--- web/src/main/java/se/inera/testsupport/RestSupportService.java
+++ web/src/main/java/se/inera/testsupport/RestSupportService.java
@@ -1,15 +1,24 @@
 package se.inera.testsupport;
 
 import org.joda.time.DateTimeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.inera.statistics.service.warehouse.NationellData;
+import se.inera.statistics.service.warehouse.Warehouse;
+import se.inera.statistics.web.service.ChartDataService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Date;
 
 @Service("restSupportService")
 public class RestSupportService {
+
+    @Autowired
+    private ChartDataService nationalChartDataService;
+
+    @Autowired
+    private Warehouse warehouse;
 
     @GET
     @Path("now")
@@ -26,5 +35,13 @@ public class RestSupportService {
         return Response.ok().build();
     }
 
+    @POST
+    @Path("recalculateNationalData")
+    @Produces({MediaType.APPLICATION_JSON })
+    public Response recalculateNationalData() {
+        warehouse.complete(warehouse.getLastUpdate().plusMillis(1));
+        nationalChartDataService.buildCache();
+        return Response.ok().build();
+    }
 
 }
