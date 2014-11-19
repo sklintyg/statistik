49bd84089e1a75150e4691e1965a01bfc6d7ef4e
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2014-11-19 16:15:26 +0100

    STATISTIK-751 Lagt till rest service för att sätta nu-tiden. Vi kom fram till att det skulle räcka med att sätta en statisk tid.

--- web/src/main/java/se/inera/testsupport/RestSupportService.java
+++ web/src/main/java/se/inera/testsupport/RestSupportService.java
@@ -1,12 +1,12 @@
 package se.inera.testsupport;
 
+import org.joda.time.DateTimeUtils;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 
 @Service("restSupportService")
 public class RestSupportService {
@@ -15,7 +15,15 @@ public class RestSupportService {
     @Path("now")
     @Produces({MediaType.APPLICATION_JSON })
     public Response getCurrentDateTime() {
-        return Response.ok(System.currentTimeMillis()).build();
+        return Response.ok(DateTimeUtils.currentTimeMillis()).build();
+    }
+
+    @POST
+    @Path("now")
+    @Produces({MediaType.APPLICATION_JSON })
+    public Response setCurrentDateTime(long timeMillis) {
+        DateTimeUtils.setCurrentMillisFixed(timeMillis);
+        return Response.ok().build();
     }
 
 
