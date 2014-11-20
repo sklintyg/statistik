e8be230527a87bb174594bd32aef5329018e9c70
Fredrik Engström <fredrik.engstrom@callistaenterprise.se>
2014-11-20 08:32:20 +0100

    STATISTIK-751 Lagt till rest-service för att nollställa databas

--- web/src/main/java/se/inera/testsupport/RestSupportService.java
+++ web/src/main/java/se/inera/testsupport/RestSupportService.java
@@ -3,10 +3,13 @@ package se.inera.testsupport;
 import org.joda.time.DateTimeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import se.inera.statistics.service.processlog.Receiver;
 import se.inera.statistics.service.warehouse.Warehouse;
 import se.inera.statistics.web.service.ChartDataService;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,16 +26,19 @@ public class RestSupportService {
     @Autowired
     private Receiver receiver;
 
+    @PersistenceContext(unitName = "IneraStatisticsLog")
+    private EntityManager manager;
+
     @GET
     @Path("now")
-    @Produces({MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_JSON})
     public Response getCurrentDateTime() {
         return Response.ok(DateTimeUtils.currentTimeMillis()).build();
     }
 
     @POST
     @Path("now")
-    @Produces({MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_JSON})
     public Response setCurrentDateTime(long timeMillis) {
         DateTimeUtils.setCurrentMillisFixed(timeMillis);
         return Response.ok().build();
@@ -40,17 +46,28 @@ public class RestSupportService {
 
     @POST
     @Path("recalculateNationalData")
-    @Produces({MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_JSON})
     public Response recalculateNationalData() {
         warehouse.complete(warehouse.getLastUpdate().plusMillis(1));
         nationalChartDataService.buildCache();
         return Response.ok().build();
     }
 
+    @POST
+    @Path("clearDatabase")
+    @Produces({MediaType.APPLICATION_JSON})
+    @Transactional
+    public Response clearDatabase() {
+        manager.createQuery("DELETE FROM IntygEvent").executeUpdate();
+        warehouse.clear();
+        recalculateNationalData();
+        return Response.ok().build();
+    }
+
     @PUT
     @Path("intyg")
-    @Consumes({MediaType.APPLICATION_JSON })
-    @Produces({MediaType.APPLICATION_JSON })
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public Response insertIntyg(Intyg intyg) {
         receiver.accept(intyg.getType(), intyg.getData(), intyg.getDocumentId(), intyg.getTimestamp());
         return Response.ok().build();
