eecc36cc7e334b764ccdb49ca9294bdbe5bfa47b
Jesper Holmberg <jesper@marbella.svr-domain.local>
2014-11-21 13:28:13 +0100

    Första implementation av REST-klient

---
+++ specifications/src/main/groovy/se/inera/statistics/web/reports/ReportsUtil.groovy
@@ -0,0 +1,37 @@
+package se.inera.statistics.web.reports
+
+import groovy.json.JsonBuilder
+import groovyx.net.http.ContentType
+import groovyx.net.http.RESTClient
+import se.inera.testsupport.Intyg
+
+class ReportsUtil {
+    def statistik = new RESTClient('http://localhost:8080/', ContentType.JSON)
+
+    long getCurrentDateTime() {
+        def response = statistik.get(path: '/api/testsupport/now')
+        response.data;
+    }
+
+    def setCurrentDateTime(long timeMillis) {
+        def response = statistik.post(path: '/api/testsupport/now', body: String.valueOf(timeMillis))
+        assert response.status == 200
+    }
+
+    def clearDatabase() {
+        def response = statistik.post(path: '/api/testsupport/clearDatabase')
+        assert response.status == 200
+    }
+
+    def insertIntyg(Intyg intyg) {
+        def builder = new JsonBuilder(intyg)
+        def response = statistik.put(path: '/api/testsupport/intyg', body: builder.toString())
+        assert response.status == 200
+    }
+
+    def processIntyg() {
+        def response = statistik.post(path: '/api/testsupport/processIntyg')
+        assert response.status == 200
+    }
+
+}
