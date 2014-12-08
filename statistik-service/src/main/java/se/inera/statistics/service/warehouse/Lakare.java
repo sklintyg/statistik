fe46f2d508a556fe36e563ed4d487bed987d9e63
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2014-12-08 15:11:42 +0100

    STATSITIK-399 Fixed issue with duplicate Lakare in Sjukfall

--- statistik-service/src/main/java/se/inera/statistics/service/warehouse/Lakare.java
+++ statistik-service/src/main/java/se/inera/statistics/service/warehouse/Lakare.java
@@ -26,4 +26,26 @@ public class Lakare {
         return age;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Lakare lakare = (Lakare) o;
+
+        if (id != lakare.id) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
