da3acf552296c9465b7b99dfd7555bd9ada2d43c
Pierre Sandboge <pierre.sandboge@standin.se>
2015-02-20 04:13:13 +0100

    STATISTIK-902 Beräkningstak work in progress

---
+++ statistik-service/src/main/java/se/inera/statistics/service/warehouse/query/CalcCoordinator.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (C) 2015 Inera AB (http://www.inera.se)
+ *
+ * This file is part of statistik (https://github.com/sklintyg/statistik).
+ *
+ * statistik is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * statistik is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.statistics.service.warehouse.query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class CalcCoordinator {
+    private static final Logger LOG = LoggerFactory.getLogger(CalcCoordinator.class);
+    private static final int EXECUTORS = Runtime.getRuntime().availableProcessors();
+    private static final int NO_OF_TICKETS = Runtime.getRuntime().availableProcessors() + 2;
+    private static final Object LOCK = new Object();
+    private static final long POLL_TIME = 100L;
+    private static final int MAX_WAIT = 5_000;
+    private static Ticket[] tickets = new Ticket[NO_OF_TICKETS];
+    private static int queueSize;
+    static {
+        for (int i = 0; i < tickets.length; i++) {
+            tickets[i] = new Ticket();
+        }
+        LOG.info("Using " +  NO_OF_TICKETS + " tickets.");
+    }
+
+    private CalcCoordinator() {
+    }
+
+    public static Ticket getTicket() {
+        Ticket returnTicket = null;
+        int size = 0;
+        synchronized (LOCK) {
+            for (Ticket ticket : tickets) {
+                if (ticket.free) {
+                    ticket.free = false;
+                    returnTicket = ticket;
+                    size = queueSize;
+                    break;
+                }
+            }
+        }
+        if (returnTicket == null) {
+            throw new CalcException("No available executors");
+        }
+        int counter = 0;
+        long start = System.currentTimeMillis();
+        while (size > EXECUTORS) {
+            synchronized (LOCK) {
+                size = queueSize;
+            }
+            LOG.info("Waited for " + counter++ + " loops");
+            if (System.currentTimeMillis() - start > MAX_WAIT) {
+                returnTicket(returnTicket);
+                throw new CalcException("Max wait time exceeded");
+            }
+            try {
+                Thread.sleep(POLL_TIME);
+            } catch (InterruptedException e) {
+                LOG.trace("Ignoring wake-up");
+            }
+        }
+        synchronized (LOCK) {
+            queueSize++;
+        }
+        return returnTicket;
+    }
+
+    public static void returnTicket(Ticket ticket) {
+        synchronized (LOCK) {
+            if (ticket != null) {
+                ticket.free = true;
+                queueSize--;
+            }
+        }
+    }
+
+    public static final class Ticket {
+        private boolean free = true;
+    }
+}
