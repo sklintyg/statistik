6e77f15e1a35b44e5dbdf749e4c1d68e87648d76
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-03-13 16:06:19 +0100

    Har tagit bort BI-export-POCen eftersom BI-export inte längre är aktuellt.

--- statistik-web/src/main/java/se/inera/testsupport/RestSupportService.java
+++ statistik-web/src/main/java/se/inera/testsupport/RestSupportService.java
@@ -1,8 +1,5 @@
 package se.inera.testsupport;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 import org.joda.time.DateTimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,35 +9,23 @@ import org.springframework.transaction.annotation.Transactional;
 import se.inera.statistics.service.hsa.HsaDataInjectable;
 import se.inera.statistics.service.processlog.LogConsumer;
 import se.inera.statistics.service.processlog.Receiver;
-import se.inera.statistics.service.warehouse.BiExportData;
-import se.inera.statistics.service.warehouse.Fact;
 import se.inera.statistics.service.warehouse.NationellData;
 import se.inera.statistics.service.warehouse.SjukfallUtil;
 import se.inera.statistics.service.warehouse.Warehouse;
 import se.inera.statistics.service.warehouse.WarehouseManager;
-import se.inera.statistics.service.warehouse.query.BiExportQuery;
 import se.inera.statistics.service.warehouse.query.CalcCoordinator;
-import se.inera.statistics.web.model.LoginInfo;
-import se.inera.statistics.web.model.Verksamhet;
 import se.inera.statistics.web.service.ChartDataService;
-import se.inera.statistics.web.service.LoginServiceUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Service("restSupportService")
 public class RestSupportService {
@@ -70,12 +55,6 @@ public class RestSupportService {
 
     @Autowired
     private HsaDataInjectable hsaDataInjectable;
-
-    @Autowired
-    private BiExportQuery biExportQuery;
-
-    @Autowired
-    private LoginServiceUtil loginServiceUtil;
 
     @Autowired
     private SjukfallUtil sjukfallUtil;
@@ -174,50 +153,5 @@ public class RestSupportService {
         hsaDataInjectable.addPersonal(personal.getId(), personal.getFirstName(), personal.getLastName(), personal.getKon(), personal.getAge(), personal.getBefattning());
         return Response.ok().build();
     }
-
-    /**
-     * This is a POC for BI export. It is located in the rest support class so that it is not exposed in production.
-     * If exposure is wanted, this endpoint should be moved to ProtectedChartDataService.
-     */
-    @GET
-    @Path("{verksamhetId}/biexport")
-    @Produces({ MediaType.APPLICATION_JSON })
-    public Response getBiExport(@Context HttpServletRequest request, @PathParam("verksamhetId") String verksamhetId) {
-        LOG.info("Calling getBiExport with verksamhetId: {}", verksamhetId);
-        Verksamhet verksamhet = getVerksamhet(request, Verksamhet.decodeId(verksamhetId));
-        final String vardgivarId = verksamhet.getVardgivarId();
-        final List<BiExportData> biExportData = biExportQuery.getBiExportData(vardgivarId, warehouse.get(vardgivarId), getFilterForAllAvailableEnhets(request));
-        return Response.ok(biExportData).build();
-    }
-
-    //Copied from ProtectedChartDataService
-    private Verksamhet getVerksamhet(HttpServletRequest request, String verksamhetId) {
-        List<Verksamhet> verksamhets = loginServiceUtil.getLoginInfo(request).getBusinesses();
-        for (Verksamhet verksamhet : verksamhets) {
-            if (verksamhet.getVardgivarId().equals(verksamhetId)) {
-                return verksamhet;
-            }
-        }
-        return null;
-    }
-
-    //Based on copy from ProtectedChartDataService
-    private Predicate<Fact> getFilterForAllAvailableEnhets(HttpServletRequest request) {
-        LoginInfo info = loginServiceUtil.getLoginInfo(request);
-        final Set<Integer> availableEnhets = new HashSet<>(Lists.transform(info.getBusinesses(), new Function<Verksamhet, Integer>() {
-            @Override
-            public Integer apply(Verksamhet verksamhet) {
-                return Warehouse.getEnhet(verksamhet.getId());
-            }
-        }));
-        return new Predicate<Fact>() {
-            @Override
-            public boolean apply(Fact fact) {
-                return availableEnhets.contains(fact.getEnhet());
-            }
-        };
-    }
-
-
 
 }
