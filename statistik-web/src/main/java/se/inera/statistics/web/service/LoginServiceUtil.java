3cc6b13b523fc97f9a916583462cbb648ac992bd
Pierre Sandboge <pierre.sandboge@standin.se>
2015-03-23 13:39:33 +0100

    STATISTIK-966 Fixade fallerande test och tog bort enhet4 från fitnesse

--- statistik-web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
+++ statistik-web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
@@ -23,8 +23,6 @@ import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.stereotype.Component;
