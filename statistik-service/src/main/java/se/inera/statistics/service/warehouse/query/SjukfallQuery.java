0cdc50b13fc5b2ebe9df17c2d4a8073bf5169f01
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-06-03 00:33:35 +0200

    STATISTIK-1055 Tröskelvärdet kommer nu att vara minst 3, oavsett vad om är angett i property-filen

--- statistik-service/src/main/java/se/inera/statistics/service/warehouse/query/SjukfallQuery.java
+++ statistik-service/src/main/java/se/inera/statistics/service/warehouse/query/SjukfallQuery.java
@@ -56,6 +56,7 @@ import java.util.Set;
 public final class SjukfallQuery {
 
     private static final Logger LOG = LoggerFactory.getLogger(SjukfallQuery.class);
+    public static final int DEFAULT_CUTOFF = 5;
 
     @Autowired
     private LakareManager lakareManager;
@@ -63,8 +64,18 @@ public final class SjukfallQuery {
     @Autowired
     private SjukfallUtil sjukfallUtil;
 
-    @Value("${reports.landsting.cutoff:5}")
-    private int cutoff;
+    private int cutoff = DEFAULT_CUTOFF;
+
+    @Autowired
+    public void initProperty(@Value("${reports.landsting.cutoff}") int cutoff) {
+        final int minimumCutoffValue = 3;
+        if (cutoff < minimumCutoffValue) {
+            LOG.warn("Landsting cutoff value is too low. Using minimum value: " + minimumCutoffValue);
+            this.cutoff = minimumCutoffValue;
+            return;
+        }
+        this.cutoff = cutoff;
+    }
 
     public SimpleKonResponse<SimpleKonDataRow> getSjukfall(Aisle aisle, SjukfallFilter filter, LocalDate start, int perioder, int periodlangd, boolean applyCutoff) {
         final Function<SjukfallGroup, String> rowNameFunction = new Function<SjukfallGroup, String>() {
