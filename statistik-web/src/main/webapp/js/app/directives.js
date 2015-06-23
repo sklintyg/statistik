ef1c91589a23c840192cbbbc6a31ac196c2e4da6
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-06-23 08:30:35 +0200

    STATISTIK-1029 Flytt av filterButton direktivet till business-filter-modulen eftersom direktivet enbart används där

--- statistik-web/src/main/webapp/js/app/directives.js
+++ statistik-web/src/main/webapp/js/app/directives.js
@@ -122,16 +122,6 @@ angular.module('StatisticsApp').directive("submenu", function (recursionService)
     };
 });
 
-angular.module('StatisticsApp').directive("filterButton", function () {
-    return {
-    	restrict: "E",
-	    template:
-	        '<button id="show-hide-filter-btn" type="button" class="btn btn-small pull-right" ng-class="{filterbtnactivefilter: filterIsActive}" ng-click="isFilterCollapsed = !isFilterCollapsed">' +
-	        '<i class="glyphicon" ng-class="{glyphiconDownSign: isFilterCollapsed, glyphiconUpSign: !isFilterCollapsed}"></i> {{!isFilterCollapsed ? "Dölj filter" : "Visa filter"}}<span style="font-size: 12px; font-style: italic;"><br/>{{filterButtonIdText}}</span><span ng-show="filterIsActive" style="font-size: 12px; font-style: italic;"><br/>Val gjorda</span>' +
-	        '</button>'
-    };
-});
-
 angular.module('StatisticsApp').directive('message',
     [ '$log', '$rootScope', 'messageService',
         function($log, $rootScope, messageService) {
--- statistik-web/src/main/webapp/js/app/shared/businessfilter/businessFilterDirective.js
+++ statistik-web/src/main/webapp/js/app/shared/businessfilter/businessFilterDirective.js
@@ -294,3 +294,14 @@ angular.module('StatisticsApp').directive('multiselectDropdown', function () {
         });
     };
 });
+
+angular.module('StatisticsApp').directive("filterButton", function () {
+    return {
+        restrict: "E",
+        template:
+        '<button id="show-hide-filter-btn" type="button" class="btn btn-small pull-right" ng-class="{filterbtnactivefilter: filterIsActive}" ng-click="isFilterCollapsed = !isFilterCollapsed">' +
+        '<i class="glyphicon" ng-class="{glyphiconDownSign: isFilterCollapsed, glyphiconUpSign: !isFilterCollapsed}"></i> {{!isFilterCollapsed ? "Dölj filter" : "Visa filter"}}<span style="font-size: 12px; font-style: italic;"><br/>{{filterButtonIdText}}</span><span ng-show="filterIsActive" style="font-size: 12px; font-style: italic;"><br/>Val gjorda</span>' +
+        '</button>'
+    };
+});
+
