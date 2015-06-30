f3ed219c086116d7e7e5b31819d328b48b93093f
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-06-30 11:21:13 +0200

    STATISTIK-1170 Se till att användaren är utloggad innan man försöker logga in via säkerhetstjänsten, detta för att undvika den oändliga loop som man annars kunde hamna i om användaren loggat in med ett uppdrag utan statistik-ändamål (och därmed skikcas direkt till statistiktjänstens loginsida efter inloggning)

---
+++ statistik-web/src/main/java/se/inera/auth/ForceLocalLogoutFilter.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015 Inera AB (http://www.inera.se)
+ *
+ * This file is part of statistik (https://github.com/sklintyg/statistik).
+ *
+ * statistik is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * statistik is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.auth;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class ForceLocalLogoutFilter extends GenericFilterBean {
+
+    /**
+     * Will clear the security context in the same way as SecurityContextLogoutHandler does it.
+     * This could for instance be used to make sure that the use will get the chance to login
+     * again at säkerhetstjänsten even when already logged in.
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.clearContext();
+        chain.doFilter(request, response);
+    }
+
+}
