ed8be50473377c82defd3a53953c0bb5bb7c8840
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-09-28 13:48:33 +0200

    Is no longer using hardcoded URL (it did not work as good in production as in test when using the hardcoded test-url)

--- tools/fileservice/src/main/java/se/inera/statistik/tools/HsaUnitSource.java
+++ tools/fileservice/src/main/java/se/inera/statistik/tools/HsaUnitSource.java
@@ -71,7 +71,7 @@ public final class HsaUnitSource {
 
             final DefaultHttpClient httpClient = new DefaultHttpClient(new PoolingClientConnectionManager(schemeRegistry), httpParams);
 
-            HttpGet httpget = new HttpGet("https://wstest.hsa.sjunet.org/hsafileservice/informationlist/hsaunits.zip");
+            HttpGet httpget = new HttpGet(url);
             HttpResponse response = httpClient.execute(httpget);
             HttpEntity entity = response.getEntity();
             if (entity != null) {
