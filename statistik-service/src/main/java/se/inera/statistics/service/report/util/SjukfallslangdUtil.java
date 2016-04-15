baef9675788311694369a6c6c3440e495181e9ba
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2016-04-15 15:36:52 +0200

    INTYG-2172 Har splittat sjukskrivningslängdsgruppen "31-90 dagar" till de två grupperna "31-60 dagar" samt "61-90 dagar". Fitnesse-specarna är uppdaterade efter bästa förmåga, men ändringarna kommer att bli verifierade av lämplig kravare i efterhand.

--- statistik-service/src/main/java/se/inera/statistics/service/report/util/SjukfallslangdUtil.java
+++ statistik-service/src/main/java/se/inera/statistics/service/report/util/SjukfallslangdUtil.java
@@ -21,7 +21,7 @@ package se.inera.statistics.service.report.util;
 import static se.inera.statistics.service.report.util.Ranges.range;
 
 public final class SjukfallslangdUtil {
-    public static final Ranges RANGES = new Ranges(range("Under 15 dagar", 15), range("15-30 dagar", 31), range("31-90 dagar", 91), range("91-180 dagar", 181), range("181-365 dagar", 366), range("Över 365 dagar", Integer.MAX_VALUE));
+    public static final Ranges RANGES = new Ranges(range("Under 15 dagar", 15), range("15-30 dagar", 31), range("31-60 dagar", 61), range("61-90 dagar", 91), range("91-180 dagar", 181), range("181-365 dagar", 366), range("Över 365 dagar", Integer.MAX_VALUE));
     private SjukfallslangdUtil() {
     }
 }
