d311dc2a6c53368da71fbd0595ed946419098f7f
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2016-09-12 00:10:21 +0200

    INTYG-2873: Fixat checkstyle-varning

--- statistik-web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
+++ statistik-web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
@@ -36,7 +36,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
