52f1dd8643cb8edda2f007d0df8d0bacc20d00df
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2016-10-14 00:44:21 +0200

    INTYG-3074: Protractor-test för att säkerställa att buggen aldrig återkommer

---
+++ specifications/src/test/protractor/dev/specs/login.spec.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*globals browser,protractor */
+/*globals pages */
+/*globals describe,it,helpers */
+'use strict';
+
+var testfw = require('../testFramework.js');
+var pages = testfw.pages;
+var features = testfw.features;
+var fakeloginPage = pages.fakeloginPo;
+var headerPage = pages.headerPo;
+
+describe('Tester kring inloggning: ', function() {
+
+    beforeEach(function() {
+        browser.get("http://localhost:8080");
+    });
+
+    it('INTYG-3074: Inloggning behålls efter sidomladdning på nationell nivå', function() {
+        features.user.makeSureNotLoggedIn();
+        features.user.loginUser1();
+        pages.navmenu.expandNationalStatisticsToggle();
+        pages.navmenu.clickNavCasesPerMonthLink();
+        browser.refresh();
+        pages.navmenu.clickBusinessStatisticsToggle();
+        pages.navmenu.clickNavVerksamhetOversiktLink();
+        expect(browser.getTitle()).toEqual('Verksamhetsöversikt | Statistiktjänsten');
+    });
+
+});
