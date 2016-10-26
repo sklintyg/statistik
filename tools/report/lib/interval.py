79a8de141e5d4e9d6bd65961c27384d7e4d2c190
Magnus Florén <magnus.floren@altran.com>
2016-10-26 13:28:47 +0200

    Change the importing of the submodules

--- tools/report/lib/interval.py
+++ tools/report/lib/interval.py
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -------------------------------------------------------------------------------
+from period import Period
 
 class Interval:
     def __init__(self, intyg):
