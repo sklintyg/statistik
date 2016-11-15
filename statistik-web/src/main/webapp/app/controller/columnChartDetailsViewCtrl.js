54e2a6ca00c2bde5b00011f0b1b02ba411b073c1
Magnus Ekstrand <magnus@ekstrand.net>
2016-11-15 10:33:44 +0100

    INTYG-3199: Rättad felstavning av scope-variabel

--- statistik-web/src/main/webapp/app/controller/columnChartDetailsViewCtrl.js
+++ statistik-web/src/main/webapp/app/controller/columnChartDetailsViewCtrl.js
@@ -84,7 +84,7 @@ angular.module('StatisticsApp').controller('columnChartDetailsViewCtrl',
                 result.allAvailableEnhetsSelectedInFilter, result.filteredEnhets,
                 result.filter.sjukskrivningslangd, result.allAvailableSjukskrivningslangdsSelectedInFilter,
                 result.filter.aldersgrupp, result.allAvailableAgeGroupsSelectedInFilter);
-            $scope.resultMessageKist = ControllerCommons.getResultMessageList(result, messageService);
+            $scope.resultMessageList = ControllerCommons.getResultMessageList(result, messageService);
             if (config.showDetailsOptions) {
                 $scope.currentPeriod = result.period;
                 statisticsData.getDiagnosisKapitelAndAvsnittAndKategori(populateDetailsOptions, function() {
