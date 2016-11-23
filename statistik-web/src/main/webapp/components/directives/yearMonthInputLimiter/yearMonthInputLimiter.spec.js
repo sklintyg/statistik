048a13af6141b341363b9a2bddc4cfaac735929a
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-11-23 14:02:48 +0100

    INTYG-3198: unittest

---
+++ statistik-web/src/main/webapp/components/directives/yearMonthInputLimiter/yearMonthInputLimiter.spec.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2013 - 2014 Inera AB (http://www.inera.se)
+ *
+ *     This file is part of Inera Statistics (http://code.google.com/p/inera-statistics/).
+ *
+ *     Inera Statistics is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Inera Statistics is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU LESSER GENERAL PUBLIC LICENSE for more details.
+ *
+ *     You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe('Tests for directive yearMonthInputLimiter', function() {
+    'use strict';
+
+    var scope, element, ngModel;
+    var ORIGINAL_VALUE = '2016-01';
+
+    beforeEach(module('StatisticsApp'));
+    beforeEach(module('htmlTemplates'));
+
+    beforeEach(inject(function($rootScope, $compile) {
+
+        scope = $rootScope.$new();
+        scope.model = {
+            dateValue: ORIGINAL_VALUE
+        };
+
+        element = $compile('<input type="text" ng-model="model.dateValue" year-month-input-limiter />')(scope);
+
+        ngModel = element.controller('ngModel');
+
+        scope.$digest();
+
+    }));
+
+    describe('verify reverting behaviour', function() {
+
+        it('should revert to original value for invalid values', function() {
+            element.val('feb').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual(ORIGINAL_VALUE);
+
+            element.val('2016-15').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual(ORIGINAL_VALUE);
+
+            element.val('0000').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual(ORIGINAL_VALUE);
+
+            element.val('2016-125').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual(ORIGINAL_VALUE);
+        });
+
+    });
+
+    describe('verify accepting behaviour', function() {
+
+        it('should keep new value when inputting new valid partial value', function() {
+            element.val('20').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual('20');
+
+        });
+
+        it('should keep new value when inputting new valid full value', function() {
+            element.val('2013-09').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual('2013-09');
+
+        });
+
+    });
+
+    describe('verify expand yyyyd behaviour', function() {
+
+        it('should insert - when entering yyyyd', function() {
+            element.val('20130').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual('2013-0');
+
+        });
+
+        it('should insert - when entering yyyydd', function() {
+            element.val('201309').trigger('input');
+            scope.$digest();
+            expect(ngModel.$viewValue).toEqual('2013-09');
+
+        });
+
+    });
+
+});
