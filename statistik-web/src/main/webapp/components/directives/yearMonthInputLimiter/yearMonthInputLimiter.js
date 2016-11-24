d7325cf85b83eddd3ef12a1572cad05a26579739
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-11-24 12:50:02 +0100

    INTYG-3198: Sökfilterdatum inmatning relaxat valideringen för att hantera fall när användaren tex skall byta årtal mitt i strängen.

--- statistik-web/src/main/webapp/components/directives/yearMonthInputLimiter/yearMonthInputLimiter.js
+++ statistik-web/src/main/webapp/components/directives/yearMonthInputLimiter/yearMonthInputLimiter.js
@@ -7,37 +7,29 @@ angular.module('StatisticsApp').directive(
                 restrict: 'A',
                 require: 'ngModel',
                 link: function(scope, element, attrs, ngModel) {
-                    var patternByLength = [
-                    /.*/i, //Anything
-                    /^[1|2]$/i, // 1 or 2
-                    /^(19|20)$/i, // 19  or 20
-                    /^(19|20)\d$/i, // 19X or 20X
-                    /^(19|20)\d{2}$/i, // 19XX or 20XX
-                    /^(19|20)\d{2}\-?$/i, // dddd and possibly a '-'- (ddddd are expanded into  dddd-d)
-                    /^(19|20)\d{2}\-(0|1)?$/i, // XXXX-X
-                    /^(19|20)\d{2}\-(0|1|0[1-9]|1[0-2])?$/i // Complete date match XXXX-XX
 
-                    ];
-                    var yearAndMorePattern = /^\d{5,6}$/i;
+                    // Partial entry pattern digts, optionally a dash, but after dash only 0 - 12
+                    var validEntry = /^[0-9]*\-?(0|1|0[1-9]|1[0-2])?$/i;
+
+                    //auto-expand pattern
+                    var yearAndMore = /^\d{5,6}$/i;
 
                     //Triggered whenever the viewValue is updated (keypress, paste etc)
                     function handleViewValueUpdate(newValue, oldValue) {
                         var hasUpdatedValue = false;
                         //If no value to work with, don't do anything
                         if (newValue) {
-                            //Expand values ddddd and dddddd to dddd-dd
-                            if (newValue.match(yearAndMorePattern)) {
+                            //1: check if match a pattern we could expand into dddd-dd
+                            if (newValue.match(yearAndMore)) {
                                 newValue = (newValue.substr(0, 4) + '-' + newValue.substr(4));
                                 hasUpdatedValue = true;
                             }
-                            //Should this new value be accepted or rejected?
-                            var patternIndexToMatch = newValue.length < patternByLength.length ? newValue.length
-                                    : patternByLength.length - 1;
-                            if (!newValue.match(patternByLength[patternIndexToMatch])) {
+                            //2: Should the new value be accepted or rejected?
+                            if (!newValue.match(validEntry)) {
                                 //Reject new value, revert to previous
                                 updateViewValue(oldValue);
                             } else {
-                                //new value is ok, but did we modifiy it here?
+                                //new value is ok, but did we modifiy it?
                                 if (hasUpdatedValue) {
                                     updateViewValue(newValue);
                                 }
