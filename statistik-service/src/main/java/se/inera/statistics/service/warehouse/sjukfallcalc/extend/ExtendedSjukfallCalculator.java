3bc5f0ed4a9ee6aa26e80dfc80ee3b0dcf93393e
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2016-11-26 01:16:32 +0100

    INTYG-3165 Refactorerat och effektiviserat ExtendedSjukfallCalculator

--- statistik-service/src/main/java/se/inera/statistics/service/warehouse/sjukfallcalc/extend/ExtendedSjukfallCalculator.java
+++ statistik-service/src/main/java/se/inera/statistics/service/warehouse/sjukfallcalc/extend/ExtendedSjukfallCalculator.java
@@ -18,50 +18,61 @@
  */
 package se.inera.statistics.service.warehouse.sjukfallcalc.extend;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Collections2;
 import se.inera.statistics.service.warehouse.Fact;
 import se.inera.statistics.service.warehouse.Sjukfall;
 import se.inera.statistics.service.warehouse.SjukfallExtended;
 
+import javax.annotation.Nonnull;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class ExtendedSjukfallCalculator {
 
-    private final ArrayListMultimap<Long, Fact> factsPerPatientInAisle;
+    static final Comparator<Fact> FACT_ORDER_BY_STARTDATE = Comparator.comparingInt(Fact::getStartdatum);
+    private final Map<Long, List<Fact>> factsPerPatientInAisle;
 
     ExtendedSjukfallCalculator(List<Fact> aisle) {
         this.factsPerPatientInAisle = getFactsPerPatient(aisle);
     }
 
-    SjukfallExtended getExtendedSjukfallStart(long patient, SjukfallExtended mergedSjukfall) {
-        return getExtendedSjukfallStart(mergedSjukfall, factsPerPatientInAisle.get(patient));
-    }
-
-    private ArrayListMultimap<Long, Fact> getFactsPerPatient(Iterable<Fact> facts) {
-        ArrayListMultimap<Long, Fact> factsPerPatient = ArrayListMultimap.create();
-        for (Fact fact : facts) {
-            factsPerPatient.put(fact.getPatient(), fact);
+    /**
+     * Will extend the startdate of the sjukfall as far back as possible using all intyg for the patient.
+     * @param patient The patient to look for when searching for more earlier intyg.
+     * @param sjukfall The sjukfall that will be extended if any suitable intygs are found.
+     * @return The final sjukfall, extended with earlier facts if possible.
+     */
+    SjukfallExtended getExtendedSjukfallStart(long patient, @Nonnull SjukfallExtended sjukfall) {
+        final List<Fact> allIntygForPatient = factsPerPatientInAisle.get(patient);
+        if (allIntygForPatient == null) {
+            return sjukfall;
         }
-        return factsPerPatient;
-    }
-
-    SjukfallExtended getExtendedSjukfallStart(final SjukfallExtended mergedSjukfall, Collection<Fact> allIntygForPatient) {
-        final Collection<Fact> extendableIntyg = Collections2.filter(allIntygForPatient, new Predicate<Fact>() {
-            @Override
-            public boolean apply(Fact fact) {
-                return fact.getStartdatum() < mergedSjukfall.getStart() && fact.getStartdatum() + fact.getSjukskrivningslangd() + Sjukfall.MAX_GAP >= mergedSjukfall.getStart();
-            }
-        });
-        if (extendableIntyg.isEmpty()) {
-            return mergedSjukfall;
-        }
-        final Fact intygForExtending = extendableIntyg.iterator().next();
-        final SjukfallExtended sjukfall = mergedSjukfall.extendSjukfallWithNewStart(intygForExtending);
-
         return getExtendedSjukfallStart(sjukfall, allIntygForPatient);
+    }
+
+    private Map<Long, List<Fact>> getFactsPerPatient(List<Fact> facts) {
+        return facts.stream().collect(Collectors.groupingBy(Fact::getPatient));
+    }
+
+    private SjukfallExtended getExtendedSjukfallStart(final SjukfallExtended sjukfall, Collection<Fact> allIntygForPatient) {
+        final List<Fact> allEarlierIntygForPatient = allIntygForPatient.stream()
+                .filter(fact -> fact.getStartdatum() < sjukfall.getStart())
+                .collect(Collectors.toList());
+        final Optional<Fact> extendableIntyg = allEarlierIntygForPatient.stream()
+                .filter(fact -> isConnectable(sjukfall, fact))
+                .min(FACT_ORDER_BY_STARTDATE);
+        if (!extendableIntyg.isPresent()) {
+            return sjukfall;
+        }
+        final SjukfallExtended mergedSjukfall = sjukfall.extendSjukfallWithNewStart(extendableIntyg.get());
+        return getExtendedSjukfallStart(mergedSjukfall, allEarlierIntygForPatient);
+    }
+
+    private boolean isConnectable(SjukfallExtended sjukfall, Fact fact) {
+        return fact.getStartdatum() + fact.getSjukskrivningslangd() + Sjukfall.MAX_GAP >= sjukfall.getStart();
     }
 
 }
--- statistik-service/src/test/java/se/inera/statistics/service/warehouse/sjukfallcalc/extend/ExtendedSjukfallCalculatorTest.java
+++ statistik-service/src/test/java/se/inera/statistics/service/warehouse/sjukfallcalc/extend/ExtendedSjukfallCalculatorTest.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ *
+ * This file is part of statistik (https://github.com/sklintyg/statistik).
+ *
+ * statistik is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * statistik is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.statistics.service.warehouse.sjukfallcalc.extend;
 
 import org.junit.Test;
@@ -9,6 +27,23 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ExtendedSjukfallCalculatorTest {
+
+    @Test
+    public void testFactOrderByStartdateComparator() throws Exception {
+        //Given
+        final ArrayList<Fact> aisle = new ArrayList<>();
+        aisle.add(createFact(70, 150, 1));
+        aisle.add(createFact(50, 180, 1));
+        aisle.add(createFact(60, 120, 1));
+
+        //When
+        aisle.sort(ExtendedSjukfallCalculator.FACT_ORDER_BY_STARTDATE);
+
+        //Then
+        assertEquals(50, aisle.get(0).getStartdatum());
+        assertEquals(60, aisle.get(1).getStartdatum());
+        assertEquals(70, aisle.get(2).getStartdatum());
+    }
 
     @Test
     public void testGetExtendedSjukfallStartEmptyAisleWillReturnUnextendedSjukfall() throws Exception {
@@ -66,6 +101,25 @@ public class ExtendedSjukfallCalculatorTest {
         aisle.add(createFact(40, 85, 1));
         aisle.add(createFact(80, 150, 1));
         aisle.add(createFact(10, 55, 1));
+        final long patient = 1L;
+        final SjukfallExtended sjukfall = new SjukfallExtended(createFact(100, 200, 1));
+
+        //When
+        final ExtendedSjukfallCalculator extendedSjukfallCalculator = new ExtendedSjukfallCalculator(aisle);
+        final SjukfallExtended result = extendedSjukfallCalculator.getExtendedSjukfallStart(patient, sjukfall);
+
+        //Then
+        assertEquals(10, result.getStart());
+        assertEquals(200, result.getEnd());
+    }
+
+    @Test
+    public void testGetExtendedSjukfallStartSeveralExtendableIntygWillExtendToEarliestStartDate() throws Exception {
+        //Given
+        final ArrayList<Fact> aisle = new ArrayList<>();
+        aisle.add(createFact(40, 185, 1));
+        aisle.add(createFact(80, 150, 1));
+        aisle.add(createFact(10, 155, 1));
         final long patient = 1L;
         final SjukfallExtended sjukfall = new SjukfallExtended(createFact(100, 200, 1));
 
