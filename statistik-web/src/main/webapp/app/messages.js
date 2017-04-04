9e48937c9531a854d827943c30e19179797057ab
Emma Molin <emma.molin@cag.se>
2017-04-04 14:22:02 +0200

    Ändring av rubriken "Andel sjukfall per kön fördelat på län" till "Könsfödelning per län". Lagt till punkter i diverse olika hjälptexter bland annat.

--- statistik-web/src/main/webapp/app/messages.js
+++ statistik-web/src/main/webapp/app/messages.js
@@ -153,7 +153,7 @@ var stMessages = {
 
         // Titles
         'title.lan': 'Antal sjukfall per 1000 invånare fördelat på län',
-        'title.lan.gender': 'Andel sjukfall per kön fördelat på län',
+        'title.lan.gender': 'Könsfördelning per län',
         'title.vardenhet': 'Antal sjukfall fördelat på vårdenhet',
         'title.sickleave': 'Antal sjukfall',
         'title.diagnosisgroup': 'Antal sjukfall fördelat på diagnosgrupp',
