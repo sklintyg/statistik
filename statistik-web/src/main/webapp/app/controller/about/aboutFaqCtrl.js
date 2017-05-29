0db9de67f039b96e6dd835df4d195e05f7820930
Emmy Damberg <emmy.damberg@cag.se>
2017-05-29 12:44:35 +0200

    Förtydligat i FAQn vad HSA-katalogen är

--- statistik-web/src/main/webapp/app/controller/about/aboutFaqCtrl.js
+++ statistik-web/src/main/webapp/app/controller/about/aboutFaqCtrl.js
@@ -69,8 +69,8 @@ angular.module('StatisticsApp').controller('aboutFaqCtrl', ['$scope',
 						'<li>Start- eller slutdatum för sjukskrivningsperioden måste vara mindre än fem år fram i tiden.</li>' +
 						'<li>Slutdatum för sjukskrivningsperioden måste vara efter startdatumet.</li>' +
 						'<li>Intyget får inte vara makulerat.</li>' +
-						'<li>Den enhet inom hälso- och sjukvården som utfärdar intyget måste finnas i HSA-katalogen.</li>' +
-						'<li>Den enhet inom hälso- och sjukvården som utfärdar intyget måste vara kopplad till en vårdgivare i HSA-katalogen.</li>' +
+						'<li>Den enhet inom hälso- och sjukvården som utfärdar intyget måste finnas i den nationella HSA-katalogen (en elektronisk katalog som innehåller kvalitetssäkrade uppgifter om bland annat enheter och personer i hälso- och sjukvården).</li>' +
+						'<li>Den enhet inom hälso- och sjukvården som utfärdar intyget måste vara kopplad till en vårdgivare i den nationella HSA-katalogen.</li>' +
 					'</ul>'
         });
         faq.push({
