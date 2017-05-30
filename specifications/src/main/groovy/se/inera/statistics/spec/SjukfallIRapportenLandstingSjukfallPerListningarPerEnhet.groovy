f1347cee6e4d2a0d59ce02f63b4f726582c3bbab
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2017-05-31 00:09:28 +0200

    INTYG-3999: Lagt till test för att verifiera uppladdningsdatum för landstingsrapporter

--- specifications/src/main/groovy/se/inera/statistics/spec/SjukfallIRapportenLandstingSjukfallPerListningarPerEnhet.groovy
+++ specifications/src/main/groovy/se/inera/statistics/spec/SjukfallIRapportenLandstingSjukfallPerListningarPerEnhet.groovy
@@ -2,10 +2,17 @@ package se.inera.statistics.spec
 
 class SjukfallIRapportenLandstingSjukfallPerListningarPerEnhet extends LandstingSjukfallPerListningarPerEnhetReport {
 
+    String fileUploadDate
+
     @Override
     public void doExecute() {
         def report = getReportSjukfallPerListningarPerEnhetLandsting()
         executeTabell(report)
+        fileUploadDate = report["fileUploadDate"]
+    }
+
+    def fileUploadDate() {
+        return fileUploadDate
     }
 
 }
