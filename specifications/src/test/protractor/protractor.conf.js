7e81203ab18f4413d3a8fc8ef4a3b47013ac5b9c
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-06-27 16:04:03 +0200

    INTYG-4178: Removed jasmine-spec-reports

--- specifications/src/test/protractor/protractor.conf.js
+++ specifications/src/test/protractor/protractor.conf.js
@@ -81,30 +81,6 @@ exports.config = {
 
         jasmine.getEnv().addReporter(screenshotReporter);
 
-        var SpecReporter = require('jasmine-spec-reporter');
-        jasmine.getEnv().addReporter(new SpecReporter({
-            displayStacktrace: 'none',      // display stacktrace for each failed assertion, values: (all|specs|summary|none)
-            displaySuccessesSummary: false, // display summary of all successes after execution
-            displayFailuresSummary: true,   // display summary of all failures after execution
-            displayPendingSummary: true,    // display summary of all pending specs after execution
-            displaySuccessfulSpec: true,    // display each successful spec
-            displayFailedSpec: true,        // display each failed spec
-            displayPendingSpec: false,      // display each pending spec
-            displaySpecDuration: false,     // display each spec duration
-            displaySuiteNumber: false,      // display each suite number (hierarchical)
-            colors: {
-                success: 'green',
-                failure: 'red',
-                pending: 'yellow'
-            },
-            prefixes: {
-                success: '✓ ',
-                failure: '✗ ',
-                pending: '* '
-            },
-            customProcessors: []
-        }));
-
         // Disable animations so e2e tests run more quickly
         var disableNgAnimate = function() {
             angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
