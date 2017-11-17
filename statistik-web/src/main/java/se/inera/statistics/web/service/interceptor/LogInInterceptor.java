941f4cca7a4ac4b14faf88a20dc6997eae2c26f8
Magnus Ekstrand <magnus@ekstrand.net>
2017-11-17 13:36:48 +0100

    DEVENV: fixed codestyle complaint...#2

--- statistik-web/src/main/java/se/inera/statistics/web/service/interceptor/LogInInterceptor.java
+++ statistik-web/src/main/java/se/inera/statistics/web/service/interceptor/LogInInterceptor.java
@@ -29,8 +29,8 @@ import static java.util.stream.Stream.of;
 
 public class LogInInterceptor extends LoggingInInterceptor {
 
-    private final static String LOOKUP_CONTENTTYPE = "Content-Type:";
-    private final static String LOOKUP_PDF = "pdf=";
+    private static final String LOOKUP_CONTENTTYPE = "Content-Type:";
+    private static final String LOOKUP_PDF = "pdf=";
 
     @Override
     protected String formatLoggingMessage(LoggingMessage loggingMessage) {
