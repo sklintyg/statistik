30d92879c27ad38c86bd735865e71b4bcdca3ffd
Magnus Ekstrand <magnus@ekstrand.net>
2018-01-15 13:47:52 +0100

    INTYG-5284: uppdatering av licens-plugin till att hantera även *.groovy och *.js

--- statistik-web/src/main/webapp/components/services/dynamicLink.service.js
+++ statistik-web/src/main/webapp/components/services/dynamicLink.service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /**
  * Links can be dynamically populated from each application's links.json file served at application startup.
  *
