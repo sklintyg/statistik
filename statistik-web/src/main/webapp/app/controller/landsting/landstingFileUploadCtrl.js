82922ac9ef79412e601131d55049075d617ae2c3
Magnus Ekstrand <magnus@ekstrand.net>
2018-01-16 11:51:25 +0100

    Revert "INTYG-5284: uppdatering av licens-plugin till att hantera även *.groovy och *.js"
    This reverts commit a3ae4df7524d87a7b3455e209250938593dee42f.

--- statistik-web/src/main/webapp/app/controller/landsting/landstingFileUploadCtrl.js
+++ statistik-web/src/main/webapp/app/controller/landsting/landstingFileUploadCtrl.js
@@ -1,21 +1,22 @@
 /*
- * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ * Copyright (C) 2013 - 2014 Inera AB (http://www.inera.se)
  *
- * This file is part of sklintyg (https://github.com/sklintyg).
+ *     This file is part of Inera Statistics (http://code.google.com/p/inera-statistics/).
  *
- * sklintyg is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *     Inera Statistics is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * sklintyg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *     Inera Statistics is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU LESSER GENERAL PUBLIC LICENSE for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 angular.module('StatisticsApp').controller('landstingFileUploadCtrl',
     [ '$scope', '$rootScope', '$timeout', '$window', 'statisticsData', 'messageService', '$location',
     function ($scope, $rootScope, $timeout, $window, statisticsData, messageService, $location) {
