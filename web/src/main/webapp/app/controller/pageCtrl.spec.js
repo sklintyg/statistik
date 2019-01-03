f1e5a2044fb97b302e191721c8eb1f016d0fb282
Andy Bandy <anders.fryksborn@gmail.com>
2019-01-03 13:38:52 +0100

    DEVENV: Uppdaterad licensheader

--- web/src/main/webapp/app/controller/pageCtrl.spec.js
+++ web/src/main/webapp/app/controller/pageCtrl.spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ * Copyright (C) 2019 Inera AB (http://www.inera.se)
  *
  *     This file is part of Inera Statistics (http://code.google.com/p/inera-statistics/).
  *
