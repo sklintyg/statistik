c6e31d59d621b1839b004020c2c096d51369b813
Peter Larsson <peter.larsson@callistaenterprise.se>
2019-01-07 14:18:11 +0100

    OPENSHIFT: demo konfiguration tillagd för OpenShift

---
+++ devops/openshift/demo/env/secret-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CATALINA_OPTS_APPEND="\
+-Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
+-Dstatistics.config.folder=/opt/$APP_NAME/config \
+-Dstatistics.config.file=/opt/$APP_NAME/config/statistik.properties \
+-Dcertificate.logback.file=/opt/$APP_NAME/config/logback-ocp.xml \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
+-Dstatistics.credentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dstatistics.resources.folder=/tmp/resources \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080"
