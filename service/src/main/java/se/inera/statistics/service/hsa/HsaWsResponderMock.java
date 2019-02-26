252090941d06eec993d4daedc1681d8c1598cb31
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2019-02-26 22:59:24 +0100

    DEVENV: Låt HSA-mocken svara tomt för frågor på privatläkare (för att få ett resultat som stämmer bättre med verkligheten när prod-dumpar processas med HSA-mocken).

--- service/src/main/java/se/inera/statistics/service/hsa/HsaWsResponderMock.java
+++ service/src/main/java/se/inera/statistics/service/hsa/HsaWsResponderMock.java
@@ -504,7 +504,7 @@ public class HsaWsResponderMock implements HsaWsResponderInterface, HsaDataInjec
     }
 
     public static boolean shouldExistInHsa(String hsaId) {
-        return hsaId != null && !hsaId.startsWith("EJHSA") && !"UTANENHETSID".equals(hsaId);
+        return hsaId != null && !hsaId.startsWith("EJHSA") && !"UTANENHETSID".equals(hsaId) && !hsaId.contains("WEBCERT");
     }
 
     private StatisticsHsaUnit createHsaUnit(HSAKey key, boolean isHuvudenhet) {
