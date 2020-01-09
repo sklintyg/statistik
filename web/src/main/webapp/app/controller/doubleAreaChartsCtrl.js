c74e629e863162158d097af6f2ebdabed37df9f4
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-01-09 11:20:43 +0100

    INTYGFV-12679: Updated license header year to 2020.

--- web/src/main/webapp/app/controller/doubleAreaChartsCtrl.js
+++ web/src/main/webapp/app/controller/doubleAreaChartsCtrl.js
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2013 - 2014 Inera AB (http://www.inera.se)
+ * Copyright (C) 2020 Inera AB (http://www.inera.se)
  *
- *     This file is part of Inera Statistics (http://code.google.com/p/inera-statistics/).
+ * This file is part of sklintyg (https://github.com/sklintyg).
  *
- *     Inera Statistics is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     Inera Statistics is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU LESSER GENERAL PUBLIC LICENSE for more details.
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* globals Highcharts */
