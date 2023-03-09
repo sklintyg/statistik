0bd5a41925f03448b7eb9915d273a12e724606e9
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-03-09 11:31:17 +0100

    INTYGFV-15391: Added mockito to build.gradle. Implemented tests for TaskCoordinatorServiceImpl.

--- web/src/test/java/se/inera/statistics/web/task/TaskCoordinatorServiceImplTest.java
+++ web/src/test/java/se/inera/statistics/web/task/TaskCoordinatorServiceImplTest.java
@@ -19,10 +19,145 @@
 
 package se.inera.statistics.web.task;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 
+@ExtendWith(MockitoExtension.class)
 class TaskCoordinatorServiceImplTest {
 
+    private static final String REDIS_CACHE_KEY = "CURRENT_TASKS";
+    private ConcurrentMapCache taskCoordinatorCache;
+
+    private ObjectMapper objectMapper;
+
+    private TaskCoordinatorServiceImpl taskCoordinatorService;
+
+    private static final String SESSION_ID = "sessionId";
+    private List request;
+
+    @Mock
+    private HttpSession session;
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @BeforeEach
+    void setUp() {
+        taskCoordinatorCache = new ConcurrentMapCache("test-cache");
+        objectMapper = new ObjectMapper();
+        taskCoordinatorService = new TaskCoordinatorServiceImpl(
+            taskCoordinatorCache,
+            objectMapper
+        );
+        request = new ArrayList<String>();
+        request.add(httpServletRequest);
+    }
+
+    @Nested
+    class UserHasNoCurrentRequestStoredInRedis {
+
+        @BeforeEach
+        void setUp() {
+            request = new ArrayList<String>();
+        }
+
+        @Test
+        void shouldReturnTaskCoordinatorAccepted() {
+            request.add(httpServletRequest);
+            when(httpServletRequest.getSession()).thenReturn(session);
+            when(session.getId()).thenReturn(SESSION_ID);
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.ACCEPTED, response);
+        }
+
+        //Possibly redundant test
+        @Test
+        void shouldReturnTaskCoordinatorAcceptedIfNoSessionWasFound() {
+            String notHttpServletRequest = "notHttpServletRequest";
+            request.add(notHttpServletRequest);
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.ACCEPTED, response);
+        }
+    }
+
+    @Nested
+    class UserHasCurrentlyOngoingRequestsStoredInRedis {
+
+        @BeforeEach
+        void setUp() {
+            when(httpServletRequest.getSession()).thenReturn(session);
+            when(session.getId()).thenReturn(SESSION_ID);
+        }
+
+        @Test
+        void shouldReturnTaskCoordinatorAcceptedIfOnlyOneRequestIsFound() throws JsonProcessingException {
+            taskCoordinatorCache.put(REDIS_CACHE_KEY, objectMapper.writeValueAsString(new ArrayList<>(Collections.singleton(SESSION_ID))));
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.ACCEPTED, response);
+        }
+
+        @Test
+        void shouldReturnTaskCoordinatorDeniedIfTwoRequestIsFound() throws JsonProcessingException {
+            final var sessionIdsCurrentlyStoredInRedis = new ArrayList<>(Arrays.asList(SESSION_ID, SESSION_ID));
+            taskCoordinatorCache.put(REDIS_CACHE_KEY, objectMapper.writeValueAsString(sessionIdsCurrentlyStoredInRedis));
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.DECLINED, response);
+        }
+    }
+
+    @Nested
+    class MultipleUsersHasyOngoingRequestsStoredInRedis {
+
+        @BeforeEach
+        void setUp() {
+            when(httpServletRequest.getSession()).thenReturn(session);
+            when(session.getId()).thenReturn(SESSION_ID);
+        }
+
+        private static final String ANOTHER_SESSION_ID = "anotherSessionId";
+
+        @Test
+        void shouldReturnTaskCoordinatorAcceptedIfOneRequestIsFoundForUser() throws JsonProcessingException {
+            final var sessionIdsCurrentlyStoredInRedis = new ArrayList<>(Arrays.asList(SESSION_ID, ANOTHER_SESSION_ID));
+            taskCoordinatorCache.put(REDIS_CACHE_KEY, objectMapper.writeValueAsString(sessionIdsCurrentlyStoredInRedis));
+
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.ACCEPTED, response);
+        }
+
+        @Test
+        void shouldReturnTaskCoordinatorAcceptedIfSessionIdsDoesNotMatchWithUser() throws JsonProcessingException {
+            final var sessionIdsCurrentlyStoredInRedis = new ArrayList<>(Arrays.asList(SESSION_ID, ANOTHER_SESSION_ID, ANOTHER_SESSION_ID));
+
+            taskCoordinatorCache.put(REDIS_CACHE_KEY, objectMapper.writeValueAsString(sessionIdsCurrentlyStoredInRedis));
+
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.ACCEPTED, response);
+        }
+
+        @Test
+        void shouldReturnTaskCoordinatorDeniedIfSessionIdsMatchWithUser() throws JsonProcessingException {
+            final var sessionIdsCurrentlyStoredInRedis = new ArrayList<>(Arrays.asList(SESSION_ID, SESSION_ID, ANOTHER_SESSION_ID));
+
+            taskCoordinatorCache.put(REDIS_CACHE_KEY, objectMapper.writeValueAsString(sessionIdsCurrentlyStoredInRedis));
+
+            final var response = taskCoordinatorService.request(request);
+            assertEquals(TaskCoordinatorResponse.DECLINED, response);
+        }
+    }
 }
