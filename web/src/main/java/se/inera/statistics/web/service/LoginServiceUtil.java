36f76311d493cc051f35d8e0b0c897f578c3069e
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2023-03-15 15:17:51 +0100

    INTYGFV-15439: Performance improvements for protected/region-services. New database index, In memory caching and pre caching of care providers. (#282)

--- web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
+++ web/src/main/java/se/inera/statistics/web/service/LoginServiceUtil.java
@@ -35,6 +35,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.saml.SAMLCredential;
@@ -122,6 +123,7 @@ public class LoginServiceUtil {
         }
     }
 
+    @Cacheable(cacheManager = "scopeRequestCacheManager", cacheNames = "default")
     public LoginInfo getLoginInfo() {
         User realUser;
         try {
